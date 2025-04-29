4871319c76cc22e5e2ddc003fc2b5adae6b2d94c
JaeHwan <wpghks56@naver.com>
2025-04-29 20:24:01 +0900

    feat: datetime extension create - 들어오는 값 비교하여 기간 반환하는 함수 - 자신의 날짜를 가져와 'month day' 로 반환하는 함수

---
+++ lib/core/extensions/datetime_extension.dart
@@ -0,0 +1,58 @@
+extension DatetimeExtension on DateTime {
+  String _monthName(int m) {
+    const full = [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December',
+    ];
+    return full[m - 1];
+  }
+
+  String _monthAbbr(int m) {
+    const a = [
+      'Jan',
+      'Feb',
+      'Mar',
+      'Apr',
+      'May',
+      'Jun',
+      'Jul',
+      'Aug',
+      'Sep',
+      'Oct',
+      'Nov',
+      'Dec',
+    ];
+    return a[m - 1];
+  }
+
+  /// 인자로 들어오는 값은 해당 값보다 이후의 날이어야합니다.
+  String formatDateRange(DateTime after) {
+    // after가 현재 날짜보다 이전이면 예외 발생
+    if (after.isBefore(this)) {
+      throw ArgumentError('인자로 들어오는 값은 해당 값보다 이후의 날이어야합니다.');
+    }
+
+    // 이제 formatDateRange 로직 구현
+    if (month != after.month) {
+      // 다른 달인 경우의 처리
+      return '${_monthName(month)} $day - ${_monthName(after.month)} ${after.day}, $year';
+    } else {
+      // 같은 달인 경우의 처리
+      return '${_monthName(month)} $day-${after.day}, $year';
+    }
+  }
+
+  String formDateString() {
+    return '${_monthAbbr(month)} $day';
+  }
+}
