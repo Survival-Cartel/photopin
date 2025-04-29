588d5bece1f92820fedfa8eba00c2c9552c803a6
cho1taeho <cow12dog@gmail.com>
2025-04-29 20:59:45 +0900

    feat: BaseIconButton 생성

---
+++ lib/presentation/component/base_icon_button.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:photopin/core/enums/button_type.dart';
+import 'package:photopin/core/styles/app_font.dart';
+
+import '../../core/styles/app_color.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            BaseIconButton(
+                buttonType: ButtonType.medium,
+                iconName: Icons.edit,
+                buttonName: 'Edit',
+                onClick: () {
+                },
+                buttonColor: AppColors.primary100
+            ),
+          ],
+        ),
+      )
+    )
+  );
+
+}
+
+
+
+class BaseIconButton extends StatelessWidget {
+  final ButtonType buttonType;
+  final IconData iconName;
+  final String buttonName;
+  final Color buttonColor;
+  final VoidCallback onClick;
+
+  const BaseIconButton({super.key, required this.buttonType, required this.iconName, required this.buttonName, required this.onClick, required this.buttonColor});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        onClick();
+      },
+      child: Container(
+        width: buttonType.width,
+        height: buttonType.height,
+        decoration: BoxDecoration(
+          color: buttonColor,
+          borderRadius: BorderRadius.circular(12)
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(iconName,
+              color: AppColors.white,
+              size: 20,
+            ),
+            SizedBox(
+              width: 8,
+            ),
+            Text(buttonName,
+              style: AppFonts.largeTextRegular.copyWith(
+                color: AppColors.white
+              ),
+            )
+          ],
+        ),
+      )
+    );
+  }
+}
