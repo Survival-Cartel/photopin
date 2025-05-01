77b3c034f093553859b5ac9478866952553ffa28
roychoi <roychoi1012@naver.com>
2025-05-01 11:30:55 +0900

    feat: 예외 처리 전까지 완료

--- lib/presentation/component/base_tab.dart
+++ lib/presentation/component/base_tab.dart
@@ -1,6 +1,7 @@
 import 'package:animated_toggle_switch/animated_toggle_switch.dart';
 import 'package:flutter/material.dart';
 
+/// [labels]와 [activeColors]의 요소의 개수는 같아야 합니다.
 class BaseTab extends StatefulWidget {
   final List<String> labels;
   final List<Color> activeColors;
@@ -33,42 +34,35 @@ class _BaseTabState extends State<BaseTab> {
     return Row(
       children: [
         Expanded(
-          child: LayoutBuilder(
-            builder: (context, constraints) {
-              final tabCount = widget.labels.length;
-              final availableWidth = constraints.maxWidth;
-              final tabWidth = availableWidth / tabCount;
-              return AnimatedToggleSwitch<int>.size(
-                textDirection: TextDirection.ltr,
-                current: currentIndex,
-                values: List.generate(tabCount, (index) => index),
-                iconOpacity: 1.0,
-                indicatorSize: Size(tabWidth, 40),
-                iconBuilder: (index) => _textBuilder(index),
-                borderWidth: 2.0,
-                iconAnimationType: AnimationType.onHover,
-                style: ToggleStyle(
-                  borderColor: Colors.transparent,
-                  borderRadius: BorderRadius.circular(30),
-                  boxShadow: const [
-                    BoxShadow(
-                      color: Colors.black12,
-                      spreadRadius: 1,
-                      blurRadius: 2,
-                      offset: Offset(0, 1.5),
-                    ),
-                  ],
+          child: AnimatedToggleSwitch<int>.size(
+            textDirection: TextDirection.ltr,
+            current: currentIndex,
+            values: List.generate(widget.labels.length, (index) => index),
+            iconOpacity: 1.0,
+            indicatorSize: const Size.fromHeight(40),
+            iconBuilder: (index) => _textBuilder(index),
+            borderWidth: 2.0,
+            iconAnimationType: AnimationType.onHover,
+            style: ToggleStyle(
+              borderColor: Colors.transparent,
+              borderRadius: BorderRadius.circular(30),
+              boxShadow: const [
+                BoxShadow(
+                  color: Colors.black12,
+                  spreadRadius: 1,
+                  blurRadius: 2,
+                  offset: Offset(0, 1.5),
                 ),
-                styleBuilder: (index) => ToggleStyle(
-                  indicatorColor: _colorBuilder(index),
-                ),
-                onChanged: (index) {
-                  setState(() {
-                    currentIndex = index;
-                  });
-                  widget.onToggle?.call(index);
-                },
-              );
+              ],
+            ),
+            styleBuilder:
+                (index) => ToggleStyle(indicatorColor: _colorBuilder(index)),
+            onChanged: (index) {
+              setState(() {
+                currentIndex = index;
+              });
+
+              widget.onToggle?.call(index);
             },
           ),
         ),
@@ -80,9 +74,10 @@ class _BaseTabState extends State<BaseTab> {
     return Center(
       child: Text(
         widget.labels[index],
-        style: const TextStyle(
+        style: TextStyle(
           fontSize: 14,
           fontWeight: FontWeight.bold,
+          color: currentIndex == index ? Colors.white : Colors.black,
         ),
       ),
     );
