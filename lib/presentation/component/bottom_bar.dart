00521701e9da4a1c28adf1ffb053b30699c8cac2
cho1taeho <cow12dog@gmail.com>
2025-05-02 14:40:35 +0900

    fix: Bottom Bar FAB 삭제

--- lib/presentation/component/bottom_bar.dart
+++ lib/presentation/component/bottom_bar.dart
@@ -2,8 +2,19 @@
 import 'package:flutter/material.dart';
 import 'package:photopin/core/styles/app_color.dart';
 import 'package:photopin/core/styles/app_font.dart';
+import 'package:photopin/presentaion/component/date_range_slider.dart';
+import 'package:photopin/presentation/component/base_button.dart';
+import 'package:photopin/presentation/component/base_icon_button.dart';
 
+import '../../core/enums/button_type.dart';
 
+void main() {
+  runApp(
+    MaterialApp(
+      home: BottomBar(),
+    )
+  );
+}
 
 
 class BottomBar extends StatefulWidget {
@@ -16,8 +27,6 @@ class BottomBar extends StatefulWidget {
 class _BottomBarState extends State<BottomBar> {
   int _selectedIndex = 0;
 
-
-  
   void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
@@ -35,18 +44,22 @@ class _BottomBarState extends State<BottomBar> {
       behavior: HitTestBehavior.opaque,
       onTap: () => _onItemTapped(index),
       child:  Container(
+        width: 48,
+        height: 56,
         child: Column(
-          mainAxisSize: MainAxisSize.min,
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Icon(
               icon, color: isSelected ? activeColor : AppColors.gray2,
             ),
-            SizedBox(
-              height: 4
-            ),
+            // SizedBox(
+            //   height: 4
+            // ),
             Text(label,
               style: AppFonts.smallerTextRegular.copyWith(
                 color: isSelected ? activeColor : AppColors.gray2,
+                height: 1.0,
               ),
             )
           ],
@@ -57,27 +70,52 @@ class _BottomBarState extends State<BottomBar> {
 
   @override
   Widget build(BuildContext context) {
-    const double barHeight = 50.0;
-    const double fabSize   = 40.0;
+    const double barHeight = 56.0;
+    const double fabSize   = 48.0;
 
     return Scaffold(
-      body: Center(child: Text('$_selectedIndex'),),
-      // floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      floatingActionButtonLocation:
-      const CustomFabLocation(overlapPercent: 2.1, barHeight: barHeight),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => _onItemTapped(2),
-        backgroundColor: AppColors.primary100,
-        shape: CircleBorder(),
-        child: Icon(Icons.camera_alt,
-          color: AppColors.white,
+      appBar: AppBar(
+        title: Text('포토 핀'),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            children: [
+              BaseButton(
+                buttonName: '은렬',
+                onClick: () {},
+                buttonType: ButtonType.big
+              ),
+              SizedBox(
+                height: 8,
+              ),
+              BaseIconButton(
+                buttonType: ButtonType.small,
+                iconName: Icons.edit,
+                buttonName: '공부하세요',
+                onClick: () {} ,
+                buttonColor: AppColors.secondary80
+              ),
+              SizedBox(
+                height: 8,
+              ),
+              DateRangeSlider(
+                startDate: DateTime(2025, 3, 25),
+                endDate: DateTime(2025, 6, 3),
+                onChanged: (DateTime sd, DateTime ed) {},
+              ),
+              SizedBox(
+                height: 8,
+              ),
+            ],
+          ),
         ),
       ),
       bottomNavigationBar: BottomAppBar(
         elevation: 8,
-          // shape: const CircularNotchedRectangle(),
-          // notchMargin: 6.0,
-          color: AppColors.white,
+        color: AppColors.white,
         child: SizedBox(
           height: barHeight,
           child: Row(
@@ -85,7 +123,27 @@ class _BottomBarState extends State<BottomBar> {
             children: [
               _buildNavItem(Icons.home, 'Home', 0, AppColors.primary100),
               _buildNavItem(Icons.map,  'Map',  1, AppColors.secondary100),
-              const SizedBox(width: fabSize),
+              GestureDetector(
+                behavior: HitTestBehavior.opaque,
+                onTap: () => _onItemTapped(2),
+                child: Container(
+                  width: fabSize,
+                  height: fabSize,
+                  decoration: BoxDecoration(
+                    color: AppColors.primary100,
+                    shape: BoxShape.circle,
+                    boxShadow: const [BoxShadow(
+                      color: Colors.black26,
+                      blurRadius: 4,
+                    )]
+                  ),
+                  child: Icon(
+                    Icons.camera_alt,
+                    size: 24,
+                    color: AppColors.white,
+                  ),
+                ),
+              ),
               _buildNavItem(Icons.photo,    'Photos',   3, AppColors.marker80),
               _buildNavItem(Icons.settings, 'Settings', 4, AppColors.marker100),
             ],
@@ -93,27 +151,5 @@ class _BottomBarState extends State<BottomBar> {
         )
       ),
     );
-  }
-}
-
-
-class CustomFabLocation extends FloatingActionButtonLocation {
-  final double overlapPercent;
-  final double barHeight;
-
-  const CustomFabLocation({
-    this.overlapPercent = 0.5,
-    required this.barHeight,
-  });
-
-  @override
-  Offset getOffset(ScaffoldPrelayoutGeometry scaffoldGeometry) {
-    final fabSize = scaffoldGeometry.floatingActionButtonSize.height;
-    final x = (scaffoldGeometry.scaffoldSize.width - fabSize) / 2;
-    final y = scaffoldGeometry.scaffoldSize.height
-        - barHeight
-        + fabSize * (1 - overlapPercent);
-
-    return Offset(x, y);
   }
 }
--- test/presentation/component/bottom_bar_test.dart
+++ test/presentation/component/bottom_bar_test.dart
@@ -1,28 +1,85 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:photopin/core/styles/app_color.dart';
 import 'package:photopin/presentation/component/bottom_bar.dart';
-
 
 void main() {
   group('Battombar 테스트', () {
-    testWidgets('Bottombar 생성이 되어야 한다', (WidgetTester tester) async {
-      await tester.pumpWidget(MaterialApp(home: BottomBar(),));
+    testWidgets('홈 Icon 클릭시 홈버튼은 primary100 나머지는 gray2 색 이어야 한다', (
+      WidgetTester tester,
+    ) async {
+      await tester.pumpWidget(MaterialApp(home: BottomBar()));
       await tester.pump();
 
-      expect(find.byType(BottomBar), findsOneWidget);
+      final homeIconFinder = find.byIcon(Icons.home);
+      expect(homeIconFinder, findsOneWidget);
+
+      final homeIcon = tester.widget<Icon>(find.byIcon(Icons.home));
+      final mapIcon = tester.widget<Icon>(find.byIcon(Icons.map));
+      final photoIcon = tester.widget<Icon>(find.byIcon(Icons.photo));
+      final settingsIcon = tester.widget<Icon>(find.byIcon(Icons.settings));
+
+      expect(homeIcon.color, AppColors.primary100);
+      expect(mapIcon.color, AppColors.gray2);
+      expect(photoIcon.color, AppColors.gray2);
+      expect(settingsIcon.color, AppColors.gray2);
     });
-    testWidgets('Bottombar 버튼이 클릭 시 변수가 변경되어야 한다', (WidgetTester tester) async {
-      await tester.pumpWidget(MaterialApp(home: BottomBar(),));
+    testWidgets('맵 Icon 클릭시 맵버튼은 secondary100 나머지는 gray2 색 이어야 한다', (
+      WidgetTester tester,
+    ) async {
+      await tester.pumpWidget(MaterialApp(home: BottomBar()));
       await tester.pump();
-      expect(find.text('0'), findsOneWidget);
 
       await tester.tap(find.byIcon(Icons.map));
       await tester.pump();
-      expect(find.text('1'), findsOneWidget);
 
-      await tester.tap(find.byIcon(Icons.camera_alt));
+      final homeIcon = tester.widget<Icon>(find.byIcon(Icons.home));
+      final mapIcon = tester.widget<Icon>(find.byIcon(Icons.map));
+      final photoIcon = tester.widget<Icon>(find.byIcon(Icons.photo));
+      final settingsIcon = tester.widget<Icon>(find.byIcon(Icons.settings));
+
+      expect(mapIcon.color, AppColors.secondary100);
+      expect(homeIcon.color, AppColors.gray2);
+      expect(photoIcon.color, AppColors.gray2);
+      expect(settingsIcon.color, AppColors.gray2);
+    });
+    testWidgets('포토 Icon 클릭시 포토버튼은 marker80 나머지는 gray2 색 이어야 한다', (
+      WidgetTester tester,
+    ) async {
+      await tester.pumpWidget(MaterialApp(home: BottomBar()));
       await tester.pump();
-      expect(find.text('2'), findsOneWidget);
+
+      await tester.tap(find.byIcon(Icons.photo));
+      await tester.pump();
+
+      final homeIcon = tester.widget<Icon>(find.byIcon(Icons.home));
+      final mapIcon = tester.widget<Icon>(find.byIcon(Icons.map));
+      final photoIcon = tester.widget<Icon>(find.byIcon(Icons.photo));
+      final settingsIcon = tester.widget<Icon>(find.byIcon(Icons.settings));
+
+      expect(photoIcon.color, AppColors.marker80);
+      expect(mapIcon.color, AppColors.gray2);
+      expect(homeIcon.color, AppColors.gray2);
+      expect(settingsIcon.color, AppColors.gray2);
+    });
+    testWidgets('세팅 Icon 클릭시 세팅버튼은 marker100 나머지는 gray2 색 이어야 한다', (
+      WidgetTester tester,
+    ) async {
+      await tester.pumpWidget(MaterialApp(home: BottomBar()));
+      await tester.pump();
+
+      await tester.tap(find.byIcon(Icons.settings));
+      await tester.pump();
+
+      final homeIcon = tester.widget<Icon>(find.byIcon(Icons.home));
+      final mapIcon = tester.widget<Icon>(find.byIcon(Icons.map));
+      final photoIcon = tester.widget<Icon>(find.byIcon(Icons.photo));
+      final settingsIcon = tester.widget<Icon>(find.byIcon(Icons.settings));
+
+      expect(settingsIcon.color, AppColors.marker100);
+      expect(mapIcon.color, AppColors.gray2);
+      expect(photoIcon.color, AppColors.gray2);
+      expect(homeIcon.color, AppColors.gray2);
     });
   });
 }
