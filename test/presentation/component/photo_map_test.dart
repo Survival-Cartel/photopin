61dd963ca61a324fc0f5b937401760a4df02ecf2
JaeHwan <wpghks56@naver.com>
2025-05-02 22:06:43 +0900

    feat: photopin map test code 작성 - 제대로 생성되어야 한다. - marker가 제대로 생성되어야 한다. - polylin이 제대로 생성되어야 한다.

---
+++ test/presentation/component/photo_map_test.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:photopin/core/styles/app_color.dart';
+import 'package:photopin/presentation/component/photopin_map.dart';
+
+void main() {
+  group('photopin map component 테스트 : ', () {
+    testWidgets('제대로 생성되어야 한다.', (tester) async {
+      await tester.pumpWidget(
+        MaterialApp(
+          home: Scaffold(
+            body: PhotoPinMap(
+              key: const ValueKey('testKey'),
+              initialLocation: LatLng(37.5125, 127.1025),
+              initialZoomLevel: 10,
+              markers: {},
+              polylines: {},
+              polyLineColor: AppColors.marker50,
+            ),
+          ),
+        ),
+      );
+      expect(find.byKey(const ValueKey('testKey')), findsOneWidget);
+    });
+    testWidgets('marker가 제대로 생성되어야 한다.', (tester) async {
+      await tester.pumpWidget(
+        MaterialApp(
+          home: Scaffold(
+            body: PhotoPinMap(
+              key: const ValueKey('testKey'),
+              initialLocation: LatLng(37.5125, 127.1025),
+              initialZoomLevel: 10,
+              markers: {
+                Marker(
+                  markerId: MarkerId('testMarker'),
+                  position: LatLng(37.5125, 127.1025),
+                ),
+              },
+              polylines: {},
+              polyLineColor: AppColors.marker50,
+            ),
+          ),
+        ),
+      );
+
+      expect(
+        find.byWidgetPredicate((w) {
+          if (w is GoogleMap) {
+            return w.markers.any((m) => m.markerId.value == 'testMarker');
+          }
+          return false;
+        }),
+        findsOneWidget,
+      );
+    });
+    testWidgets('polyline이 제대로 생성되어야 한다.', (tester) async {
+      await tester.pumpWidget(
+        MaterialApp(
+          home: Scaffold(
+            body: PhotoPinMap(
+              key: const ValueKey('testKey'),
+              initialLocation: LatLng(37.5125, 127.1025),
+              initialZoomLevel: 10,
+              markers: {
+                Marker(
+                  markerId: MarkerId('testMarker'),
+                  position: LatLng(37.5125, 127.1025),
+                ),
+              },
+              polylines: {
+                'testPolyline': [
+                  LatLng(37.5125, 127.1025),
+                  LatLng(37.5123, 127.1021),
+                ],
+              },
+              polyLineColor: AppColors.marker50,
+            ),
+          ),
+        ),
+      );
+
+      expect(
+        find.byWidgetPredicate((w) {
+          if (w is GoogleMap) {
+            return w.polylines.any((m) => m.polylineId.value == 'testPolyline');
+          }
+          return false;
+        }),
+        findsOneWidget,
+      );
+    });
+  });
+}
