abeffea7dc93e76a5e9505ad77bccdf84ab0ccdb
JaeHwan <wpghks56@naver.com>
2025-05-07 14:42:51 +0900

    fix: import error 제거

--- test/presentation/component/journey_card_image_test.dart
+++ test/presentation/component/journey_card_image_test.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:network_image_mock/network_image_mock.dart';
 import 'package:photopin/presentation/component/journal_card_image.dart';
-import 'package:photopin/presentation/component/journey_card_image.dart';
 
 void main() {
   group('share journey card 컴포넌트 테스트', () {
