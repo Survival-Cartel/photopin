f40a2feb6e9777fdcf51340ac46b4cff96f4220e
roychoi1012 <roychoi1012@naver.com>
2025-05-08 16:23:53 +0900

    feat: journal data source 구현

---
+++ lib/journal/data/data_source/journal_data_source.dart
@@ -0,0 +1,9 @@
+import 'package:photopin/journal/data/dto/journal_dto.dart';
+
+abstract interface class JournalDataSource {
+  Future<JournalDto> findJournalById(String id);
+  Future<JournalDto> findJournalByName(String name);
+  Future<void> saveJournal(JournalDto dto);
+  Future<void> deleteJournal(String journalId);
+  Future<List<JournalDto>> findJournals();
+}
---
+++ lib/journal/data/data_source/journal_data_source_impl.dart
@@ -0,0 +1,74 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:photopin/core/errors/firestore_error.dart';
+import 'package:photopin/journal/data/data_source/journal_data_source.dart';
+import 'package:photopin/journal/data/dto/journal_dto.dart';
+
+class JournalDataSourceImpl implements JournalDataSource {
+  final CollectionReference<JournalDto> journalStore;
+
+  JournalDataSourceImpl({required this.journalStore});
+
+  @override
+  Future<JournalDto> findJournalById(String id) async {
+    final QuerySnapshot<JournalDto> snapshot = await journalStore
+        .where('id', isEqualTo: id)
+        .get()
+        .timeout(
+          const Duration(seconds: 8),
+          onTimeout: () => throw FirestoreError.timeOutError,
+        );
+
+    for (final docSnapshot in snapshot.docs) {
+      return docSnapshot.data();
+    }
+
+    throw FirestoreError.notFoundError;
+  }
+
+  @override
+  Future<JournalDto> findJournalByName(String name) async {
+    final QuerySnapshot<JournalDto> snapshot = await journalStore
+        .where('name', isEqualTo: name)
+        .get()
+        .timeout(
+          const Duration(seconds: 8),
+          onTimeout: () => throw FirestoreError.timeOutError,
+        );
+
+    for (final docSnapshot in snapshot.docs) {
+      return docSnapshot.data();
+    }
+
+    throw FirestoreError.notFoundError;
+  }
+
+  @override
+  Future<void> deleteJournal(String journalId) async {
+    await journalStore.doc(journalId).delete();
+  }
+
+  @override
+  Future<void> saveJournal(JournalDto dto) async {
+    final DocumentReference<JournalDto> journaldoc = await journalStore.add(
+      dto,
+    );
+    final String docId = journaldoc.id;
+
+    await journalStore.doc(docId).update({'id': docId});
+  }
+
+  @override
+  Future<List<JournalDto>> findJournals() async {
+    try {
+      final snapshot = await journalStore.get().timeout(
+        const Duration(seconds: 8),
+        onTimeout: () => throw FirestoreError.timeOutError,
+      );
+
+      return snapshot.docs.map((doc) => doc.data()).toList();
+    } catch (e) {
+      // 필요 시 다른 에러 핸들링 가능
+      rethrow;
+    }
+  }
+}
