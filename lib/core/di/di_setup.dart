a89c555fb82b2c72aa1aaf2dbb26fb642ec7ae00
JaeHwan <wpghks56@naver.com>
2025-05-08 10:12:10 +0900

    add: firestore stup 생성에 맞게 di에 추가

--- lib/core/di/di_setup.dart
+++ lib/core/di/di_setup.dart
@@ -1,5 +1,8 @@
 import 'package:get_it/get_it.dart';
+import 'package:photopin/core/firebase/firestore_setup.dart';
 
 final getIt = GetIt.instance;
 
-void di() {}
+void di() {
+  getIt.registerLazySingleton<FirestoreSetup>(() => FirestoreSetup());
+}
