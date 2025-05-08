eacfed56ff3e5cafddf9647cc67190afe90a4756
JaeHwan <wpghks56@naver.com>
2025-05-08 20:57:20 +0900

    add: photo data source & photo repository di 추가

--- lib/core/di/di_setup.dart
+++ lib/core/di/di_setup.dart
@@ -1,8 +1,22 @@
 import 'package:get_it/get_it.dart';
 import 'package:photopin/core/firebase/firestore_setup.dart';
+import 'package:photopin/photo/data/data_source/photo_data_source.dart';
+import 'package:photopin/photo/data/data_source/photo_data_source_impl.dart';
+import 'package:photopin/photo/data/repository/photo_repository.dart';
+import 'package:photopin/photo/data/repository/photo_repository_impl.dart';
 
 final getIt = GetIt.instance;
 
 void di() {
   getIt.registerLazySingleton<FirestoreSetup>(() => FirestoreSetup());
+
+  // userId 마다 firestore에서 받아오는 photo collection 이 달라져야함으로 싱글톤이 의미가 없음.
+  getIt.registerFactoryParam<PhotoDataSource, String, void>(
+    (userId, _) => PhotoDataSourceImpl(
+      photoStore: getIt<FirestoreSetup>().photoFirestore(userId),
+    ),
+  );
+  getIt.registerLazySingleton<PhotoRepository>(
+    () => PhotoRepositoryImpl(dataSource: getIt()),
+  );
 }
