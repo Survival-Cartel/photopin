37f17f6293e16d78ca66138b9fcf81ba5dc169bd
cho1taeho <cow12dog@gmail.com>
2025-05-09 16:14:11 +0900

    fix: journalFixtures 오타수정

--- test/journal/fixtures/journal_model_fixtures.dart
+++ test/journal/fixtures/journal_model_fixtures.dart
@@ -2,7 +2,7 @@ import 'package:photopin/journal/domain/model/journal_model.dart';
 
 final JournalModel journalModelFixture = JournalModel(
   id: '5',
-  name: 'japen',
+  name: 'japan',
   tripWith: ['jay', 'roy', 'steroy'],
   startDate: DateTime(2025, 5, 1),
   endDate: DateTime(2025, 5, 10),
