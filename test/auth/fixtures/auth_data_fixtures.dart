27abd18826e8a81d8a6f14ca0a926c6cada0ffc1
JaeHwan <wpghks56@naver.com>
2025-05-09 10:50:40 +0900

    test: auth test 코드에서 사용될 데이터 정의

---
+++ test/auth/fixtures/auth_data_fixtures.dart
@@ -0,0 +1,9 @@
+import 'package:photopin/user/data/dto/user_dto.dart';
+
+final List<UserDto> authDataFixtures = [];
+const UserDto authFixture = UserDto(
+  id: 'testUserId',
+  email: 'yujehwan808@gmail.com',
+  profileImg: '',
+  displayName: '유제환',
+);
