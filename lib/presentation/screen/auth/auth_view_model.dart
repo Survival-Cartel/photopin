2ec80d0329791a850feec62a0207cd27745f363b
JaeHwan <wpghks56@naver.com>
2025-05-09 15:51:15 +0900

    feat: auth view model 생성

---
+++ lib/presentation/screen/auth/auth_view_model.dart
@@ -0,0 +1,71 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:photopin/auth/data/repository/auth_repository.dart';
+import 'package:photopin/presentation/screen/auth/auth_action.dart';
+import 'package:photopin/presentation/screen/auth/auth_state.dart';
+
+class AuthViewModel with ChangeNotifier {
+  final AuthRepository _authRepository;
+  AuthState _state = const AuthState();
+  final _eventController = StreamController<Exception?>();
+
+  Stream<Exception?> get errorStream => _eventController.stream;
+
+  @override
+  void dispose() {
+    _eventController.close();
+    super.dispose();
+  }
+
+  AuthState get state => _state;
+
+  AuthViewModel(this._authRepository);
+
+  Future<void> action(AuthAction action) async {
+    switch (action) {
+      case Login():
+        await _login();
+      case OnClick():
+        break;
+      case Logout():
+        await _logout();
+    }
+  }
+
+  Future<void> _login() async {
+    try {
+      _state = state.copyWith(isLoading: true);
+      notifyListeners();
+
+      await _authRepository.login();
+      _state = state.copyWith(
+        email: (await _authRepository.findCurrentUser()).email,
+      );
+    } on Exception catch (e) {
+      _addError(e);
+    } finally {
+      _state = state.copyWith(isLoading: false);
+      notifyListeners();
+    }
+  }
+
+  Future<void> _logout() async {
+    try {
+      _state = state.copyWith(isLoading: true);
+      notifyListeners();
+
+      await _authRepository.logout();
+      _state = state.copyWith(email: '');
+    } on Exception catch (e) {
+      _addError(e);
+    } finally {
+      _state = state.copyWith(isLoading: false);
+      notifyListeners();
+    }
+  }
+
+  void _addError(Exception e) {
+    _eventController.add(e);
+  }
+}
