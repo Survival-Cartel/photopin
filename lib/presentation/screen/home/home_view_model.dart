7355e771b2000d3a3509f0393d0e71693cc00479
roychoi1012 <roychoi1012@naver.com>
2025-05-09 21:47:35 +0900

    feat: home view model에 getCurrentUserUseCase 적용

--- lib/presentation/screen/home/home_view_model.dart
+++ lib/presentation/screen/home/home_view_model.dart
@@ -1,22 +1,21 @@
 import 'package:flutter/widgets.dart';
-import 'package:photopin/auth/data/repository/auth_repository.dart';
+import 'package:photopin/core/usecase/get_current_user_use_case.dart';
 import 'package:photopin/presentation/screen/home/home_action.dart';
 import 'package:photopin/presentation/screen/home/home_state.dart';
 import 'package:photopin/user/domain/model/user_model.dart';
 
 class HomeViewModel with ChangeNotifier {
-  final AuthRepository _authRepository;
+  final GetCurrentUserUseCase getCurrentUserUseCase;
   HomeState _homeState = const HomeState();
 
-  HomeViewModel({required AuthRepository authRepository})
-    : _authRepository = authRepository;
+  HomeViewModel({required this.getCurrentUserUseCase});
 
   HomeState get homeState => _homeState;
 
   Future<void> _findUser() async {
     _homeState = homeState.copyWith(isLoading: true);
     notifyListeners();
-    final UserModel user = await _authRepository.findCurrentUser();
+    final UserModel user = await getCurrentUserUseCase.execute();
     _homeState = homeState.copyWith(
       isLoading: false,
       userName: user.displayName,
