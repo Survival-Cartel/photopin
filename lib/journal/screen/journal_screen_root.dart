8e1cfd93455f040f263902c46dc6307d231b7c02
Junsung Choi <choiexe1@gmail.com>
2025-05-09 15:41:58 +0900

    feat: journal screen root으로 MVI 패턴 적용

---
+++ lib/journal/screen/journal_screen_root.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/widgets.dart';
+import 'package:photopin/journal/screen/journal_screen.dart';
+import 'package:photopin/journal/screen/journal_screen_action.dart';
+import 'package:photopin/journal/screen/journal_screen_view_model.dart';
+
+class JournalScreenRoot extends StatefulWidget {
+  final JournalScreenViewModel viewModel;
+
+  const JournalScreenRoot({super.key, required this.viewModel});
+
+  @override
+  State<StatefulWidget> createState() => _JournalScreenRootState();
+}
+
+class _JournalScreenRootState extends State<JournalScreenRoot> {
+  @override
+  void initState() {
+    super.initState();
+    widget.viewModel.init();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListenableBuilder(
+      listenable: widget.viewModel,
+      builder: (context, child) {
+        return JournalScreen(
+          state: widget.viewModel.state,
+          onAction: (JournalScreenAction action) async {
+            switch (action) {
+              case SearchJournal():
+                await widget.viewModel.onAction(action);
+              case OnTapJournalCard():
+              // 추후 go_router로 화면 이동 로직
+            }
+          },
+        );
+      },
+    );
+  }
+}
