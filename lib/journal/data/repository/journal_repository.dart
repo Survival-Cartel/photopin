d477588dbcb0a8c62d0908cb256d7561b01954ed
cho1taeho <cow12dog@gmail.com>
2025-05-09 15:36:34 +0900

    feat: JournalRepositoryTest 코드 구현

--- lib/journal/data/repository/journal_repository.dart
+++ lib/journal/data/repository/journal_repository.dart
@@ -5,11 +5,7 @@ import '../dto/journal_dto.dart';
 
 abstract interface class JournalRepository
     implements Repository<JournalModel, String, JournalDto> {
-  Future<JournalModel> findJournalById(String id);
-
   Future<void> deleteJournal(String journalId);
 
-  Future<void> saveJournal(JournalModel model);
-
-  Future<List<JournalModel>> findJournals();
+  Future<void> saveJournal(JournalDto dto);
 }
--- lib/journal/data/repository/journal_repository_impl.dart
+++ lib/journal/data/repository/journal_repository_impl.dart
@@ -3,28 +3,15 @@ import 'package:photopin/journal/data/mapper/journal_mapper.dart';
 import 'package:photopin/journal/data/repository/journal_repository.dart';
 import 'package:photopin/journal/domain/model/journal_model.dart';
 
+import '../dto/journal_dto.dart';
+
 class JournalRepositoryImpl implements JournalRepository {
   final JournalDataSource dataSource;
 
   const JournalRepositoryImpl({required this.dataSource});
 
   @override
-  Future<void> deleteJournal(String journalId) async {
-    await dataSource.deleteJournal(journalId);
-  }
-
-  @override
   Future<List<JournalModel>> findAll() async {
-    return (await dataSource.findJournals()).map((e) => e.toModel()).toList();
-  }
-
-  @override
-  Future<JournalModel> findJournalById(String id) async {
-    return (await dataSource.findJournalById(id)).toModel();
-  }
-
-  @override
-  Future<List<JournalModel>> findJournals() async {
     return (await dataSource.findJournals()).map((e) => e.toModel()).toList();
   }
 
@@ -34,7 +21,12 @@ class JournalRepositoryImpl implements JournalRepository {
   }
 
   @override
-  Future<void> saveJournal(JournalModel model) async {
-    await dataSource.saveJournal(model.toDto());
+  Future<void> saveJournal(JournalDto dto) async {
+    await dataSource.saveJournal(dto);
+  }
+
+  @override
+  Future<void> deleteJournal(String journalId) async {
+    await dataSource.deleteJournal(journalId);
   }
 }
