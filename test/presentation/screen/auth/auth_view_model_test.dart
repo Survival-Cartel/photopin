e3aca8958b3cfc7697462d25182b81d4b17a08a8
Junsung Choi <choiexe1@gmail.com>
2025-05-10 15:59:17 +0900

    rename: test auth_view_model.dart -> auth_view_model_test.dart

--- test/presentation/screen/auth/auth_view_model_test.dart
+++ test/presentation/screen/auth/auth_view_model_test.dart
@@ -6,6 +6,7 @@ import 'package:photopin/presentation/screen/auth/auth_action.dart';
 import 'package:photopin/presentation/screen/auth/auth_state.dart';
 import 'package:photopin/presentation/screen/auth/auth_view_model.dart';
 import 'package:photopin/user/data/mapper/user_mapper.dart';
+import 'package:photopin/user/domain/model/user_model.dart';
 
 import '../../../auth/data/data_source/fake_auth_data_source.dart';
 import '../../../auth/fixtures/auth_data_fixtures.dart';
@@ -24,18 +25,22 @@ void main() {
   test('뷰모델의 초기 상태(state)는 isLoading이 false이고 email이 비어 있어야 한다.', () {
     expect(viewModel.state, isA<AuthState>());
     expect(viewModel.state.isLoading, false);
-    expect(viewModel.state.email, '');
+    expect(
+      viewModel.state.currentUser,
+      const UserModel(displayName: '', email: '', id: '', profileImg: ''),
+    );
   });
+
   group('action() : ', () {
     test('action(AuthAction.login()) 은 login() 액션을 트리거 해야한다.', () async {
       await viewModel.action(AuthAction.login());
 
-      expect(viewModel.state.email, authFixture.toModel().email);
+      expect(viewModel.state.currentUser.email, authFixture.toModel().email);
     });
     test('action(AuthAction.logout()) 은 logout() 액션을 트리거 해야한다.', () async {
       await viewModel.action(AuthAction.logout());
 
-      expect(viewModel.state.email, '');
+      expect(viewModel.state.currentUser.email, '');
     });
   });
 }
