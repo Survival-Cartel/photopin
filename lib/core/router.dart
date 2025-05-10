7701b0c4b49f6324f7278997477a55009783cd6b
Junsung Choi <choiexe1@gmail.com>
2025-05-10 15:48:26 +0900

    feat: login route 추가 및 journal 라우트가 id 파라미터를 handle 하도록 변경

--- lib/core/router.dart
+++ lib/core/router.dart
@@ -2,21 +2,36 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:photopin/core/di/di_setup.dart';
 import 'package:photopin/core/routes.dart';
+import 'package:photopin/presentation/screen/auth/auth_screen_root.dart';
+import 'package:photopin/presentation/screen/auth/auth_view_model.dart';
 import 'package:photopin/presentation/screen/home/home_screen_root.dart';
+import 'package:photopin/presentation/screen/home/home_view_model.dart';
 import 'package:photopin/presentation/screen/journal/journal_screen_root.dart';
+import 'package:photopin/presentation/screen/journal/journal_screen_view_model.dart';
 
 final appRouter = GoRouter(
+  initialLocation: Routes.login,
   routes: <RouteBase>[
     GoRoute(
       path: Routes.home,
       builder: (BuildContext context, GoRouterState state) {
-        return HomeScreenRoot(homeViewModel: getIt());
+        return HomeScreenRoot(viewModel: getIt<HomeViewModel>());
       },
     ),
     GoRoute(
       path: Routes.journal,
       builder: (BuildContext context, GoRouterState state) {
-        return JournalScreenRoot(viewModel: getIt());
+        final String userId = state.pathParameters['id']!;
+
+        return JournalScreenRoot(
+          viewModel: getIt<JournalScreenViewModel>(param1: userId),
+        );
+      },
+    ),
+    GoRoute(
+      path: Routes.login,
+      builder: (BuildContext context, GoRouterState state) {
+        return AuthScreenRoot(authViewModel: getIt<AuthViewModel>());
       },
     ),
   ],
