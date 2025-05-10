ce0044c19695f5ef7979159b64e00e82d031fcc4
cho1taeho <cow12dog@gmail.com>
2025-05-10 14:16:19 +0900

    fix: 에러코드 제거

--- lib/presentation/screen/main/main_screen.dart
+++ lib/presentation/screen/main/main_screen.dart
@@ -1,31 +1,7 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:photopin/core/di/di_setup.dart';
 import 'package:photopin/presentation/component/bottom_bar.dart';
 import 'package:photopin/presentation/component/top_bar.dart';
-
-import '../../../core/router.dart';
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  di();
-  runApp(const PhotopinApp());
-}
-
-class PhotopinApp extends StatelessWidget {
-  const PhotopinApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: appRouter, // ← GoRouter will create the shell
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(/* … */),
-    );
-  }
-}
 
 class MainScreen extends StatefulWidget {
   final StatefulNavigationShell navigationShell;
