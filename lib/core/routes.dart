a2af16b3f42b122c68f3f19b1fb393547c5ccf62
Junsung Choi <choiexe1@gmail.com>
2025-05-10 15:47:46 +0900

    feat: journal 라우트에 id 파라미터 추가

--- lib/core/routes.dart
+++ lib/core/routes.dart
@@ -1,6 +1,6 @@
 abstract class Routes {
   static const String home = '/';
-  static const String journal = '/journals';
+  static const String journal = '/journals/:id';
   static const String photos = '/photos';
   static const String login = '/login';
 }
