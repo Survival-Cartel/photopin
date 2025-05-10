6a6e5ab2b6d595413728eae242ef54cf8586efe9
Junsung Choi <choiexe1@gmail.com>
2025-05-10 20:28:49 +0900

    feat: GetJournalListUseCase가 JournalPhotoCollection을 반환하도록 수정

--- lib/core/usecase/get_journal_list_use_case.dart
+++ lib/core/usecase/get_journal_list_use_case.dart
@@ -1,13 +1,31 @@
+import 'package:photopin/core/domain/journal_photo_collection.dart';
 import 'package:photopin/journal/data/repository/journal_repository.dart';
 import 'package:photopin/journal/domain/model/journal_model.dart';
+import 'package:photopin/photo/data/repository/photo_repository.dart';
+import 'package:photopin/photo/domain/model/photo_model.dart';
 
 class GetJournalListUseCase {
   final JournalRepository _journalRepository;
+  final PhotoRepository _photoRepository;
 
-  const GetJournalListUseCase(this._journalRepository);
+  const GetJournalListUseCase({
+    required JournalRepository journalRepository,
+    required PhotoRepository photoRepository,
+  }) : _journalRepository = journalRepository,
+       _photoRepository = photoRepository;
 
-  Future<List<JournalModel>> execute() async {
+  Future<JournalPhotoCollection> execute() async {
     List<JournalModel> journals = await _journalRepository.findAll();
-    return journals;
+
+    final Map<String, List<PhotoModel>> photoMap = {};
+
+    for (final journal in journals) {
+      final List<PhotoModel> photos = await _photoRepository
+          .findPhotosByJournalId(journal.id);
+
+      photoMap[journal.id] = photos;
+    }
+
+    return JournalPhotoCollection(journals: journals, photoMap: photoMap);
   }
 }
