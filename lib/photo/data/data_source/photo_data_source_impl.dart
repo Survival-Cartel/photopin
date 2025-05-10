126e0d0ee9fba25b4b633969953131b39d7750a5
JaeHwan <wpghks56@naver.com>
2025-05-10 18:43:53 +0900

    fix: 데이터 제대로 가져올 수 있도록 then 으로 변경

--- lib/photo/data/data_source/photo_data_source_impl.dart
+++ lib/photo/data/data_source/photo_data_source_impl.dart
@@ -29,12 +29,17 @@ class PhotoDataSourceImpl implements PhotoDataSource {
   Future<List<PhotoDto>> findPhotos() async {
     final List<PhotoDto> photoDtos = [];
 
-    final QuerySnapshot<PhotoDto> snapshot = await photoStore.get().timeout(
-      const Duration(seconds: 8),
-      onTimeout: () => throw FirestoreError.timeOutError,
-    );
-
-    snapshot.docs.map((e) => photoDtos.add(e.data()));
+    await photoStore
+        .get()
+        .then((snapshot) {
+          for (var doc in snapshot.docs) {
+            photoDtos.add(doc.data());
+          }
+        })
+        .timeout(
+          const Duration(seconds: 8),
+          onTimeout: () => throw FirestoreError.timeOutError,
+        );
 
     return photoDtos;
   }
