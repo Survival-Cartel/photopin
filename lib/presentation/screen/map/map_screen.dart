e92a3f16ed2614e5e7c588fa037da13cfcaa621c
JaeHwan <wpghks56@naver.com>
2025-05-10 12:48:33 +0900

    add: map 데이터 로딩 화면 추가

--- lib/presentation/screen/map/map_screen.dart
+++ lib/presentation/screen/map/map_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:photopin/core/styles/app_color.dart';
 import 'package:photopin/presentation/component/photopin_map.dart';
 import 'package:photopin/presentation/screen/map/map_action.dart';
 import 'package:photopin/presentation/screen/map/map_state.dart';
@@ -12,15 +13,42 @@ class MapScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return PhotoPinMap(
-      initialLocation: LatLng(
-        mapState.photos.first.location.latitude,
-        mapState.photos.first.location.longitude,
-      ),
-      initialZoomLevel: 13,
-      markers: null,
-      polylines: {},
-      polyLineColor: null,
+    return Scaffold(
+      body:
+          !mapState.isLoading
+              ? PhotoPinMap(
+                initialLocation: LatLng(
+                  mapState.photos.first.location.latitude,
+                  mapState.photos.first.location.longitude,
+                ),
+                initialZoomLevel: 16,
+                markers:
+                    mapState.photos.map((photo) {
+                      return Marker(
+                        markerId: MarkerId(photo.id),
+                        position: LatLng(
+                          photo.location.latitude,
+                          photo.location.longitude,
+                        ),
+                        onTap: () {
+                          onAction(MapAction.onPhotoClick(photo.id));
+                        },
+                      );
+                    }).toSet(),
+                polylines: {
+                  mapState.journal.name:
+                      mapState.photos
+                          .map(
+                            (photo) => LatLng(
+                              photo.location.latitude,
+                              photo.location.longitude,
+                            ),
+                          )
+                          .toList(),
+                },
+                polyLineColor: AppColors.marker70,
+              )
+              : const CircularProgressIndicator(),
     );
   }
 }
