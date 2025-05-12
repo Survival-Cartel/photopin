9a247d3aec2ca10fbf63b07dc37947cd0e500416
Junsung Choi <choiexe1@gmail.com>
2025-05-12 23:02:07 +0900

    feat: uploadFile 수정 및 download 기능 제거

--- lib/storage/data/data_source/storage_data_source.dart
+++ lib/storage/data/data_source/storage_data_source.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/services.dart';
 
 abstract interface class StorageDataSource {
-  Future<void> uploadFile(String path, Uint8List bytes, String? contentType);
-  Future<String> getDownloadUrl(String path);
+  Future<String> uploadFile(String path, Uint8List bytes, String? contentType);
 }
