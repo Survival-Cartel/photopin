a77d2412a280d7b2e270507ec708454a77cb2174
Junsung Choi <choiexe1@gmail.com>
2025-05-12 20:58:40 +0900

    style: 포매팅

--- lib/core/usecase/permission_checker_use_case.dart
+++ lib/core/usecase/permission_checker_use_case.dart
@@ -1,13 +1,11 @@
 import 'package:permission_handler/permission_handler.dart';
 import 'package:photopin/presentation/screen/camera/handler/permission_checker.dart';
 
-
 class PermissionCheckerUseCase {
   final PermissionChecker _permissionChecker;
 
   const PermissionCheckerUseCase({required PermissionChecker permissionChecker})
-      : _permissionChecker = permissionChecker;
-
+    : _permissionChecker = permissionChecker;
 
   Future<void> execute(Permission permission) async {
     await _permissionChecker.check(permission);
