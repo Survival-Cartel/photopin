4880694f492193798e1be7e3e9be4abb2a7f5102
Junsung Choi <choiexe1@gmail.com>
2025-05-12 20:59:16 +0900

    feat: 임시 기능

--- lib/presentation/screen/camera/handler/image_picker_camera_handler.dart
+++ lib/presentation/screen/camera/handler/image_picker_camera_handler.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:image_gallery_saver_plus/image_gallery_saver_plus.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:photopin/core/domain/binary_data.dart';
 import 'package:photopin/presentation/screen/camera/handler/camera_handler.dart';
 
 class ImagePickerCameraHandler implements CameraHandler {
@@ -9,16 +10,20 @@ class ImagePickerCameraHandler implements CameraHandler {
   ImagePickerCameraHandler();
 
   @override
-  Future<Uint8List?> launch() async {
+  Future<BinaryData?> launch() async {
     XFile? image = await _imagePicker.pickImage(source: ImageSource.camera);
+
+    debugPrint('MIME TYPE ::: ${image?.mimeType}');
+    debugPrint('NAME ::: ${image?.name}');
 
     if (image != null) {
       Uint8List imageBytes = await image.readAsBytes();
 
-      final result = await ImageGallerySaverPlus.saveFile(image.path);
+      final BinaryData binary = BinaryData(bytes: imageBytes, mimeType: 'jpg');
 
+      final result = await ImageGallerySaverPlus.saveFile(image.path);
       debugPrint("[Launcher] 갤러리 저장 결과: $result");
-      return imageBytes;
+      return binary;
     } else {
       debugPrint("[Launcher] 사진 촬영 취소");
       return null;
