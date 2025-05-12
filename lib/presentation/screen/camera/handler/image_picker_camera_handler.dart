febe5887eef00c89911275cdb9580451020068f5
Junsung Choi <choiexe1@gmail.com>
2025-05-12 16:05:39 +0900

    feat: image picker camera handler implements

---
+++ lib/presentation/screen/camera/handler/image_picker_camera_handler.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/foundation.dart';
+import 'package:image_gallery_saver_plus/image_gallery_saver_plus.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:photopin/presentation/screen/camera/handler/camera_handler.dart';
+
+class ImagePickerCameraHandler implements CameraHandler {
+  final ImagePicker _imagePicker = ImagePicker();
+
+  ImagePickerCameraHandler();
+
+  @override
+  Future<Uint8List?> launch() async {
+    XFile? image = await _imagePicker.pickImage(source: ImageSource.camera);
+
+    if (image != null) {
+      Uint8List imageBytes = await image.readAsBytes();
+
+      final result = await ImageGallerySaverPlus.saveFile(image.path);
+
+      debugPrint("[Launcher] 갤러리 저장 결과: $result");
+      return imageBytes;
+    } else {
+      debugPrint("[Launcher] 사진 촬영 취소");
+      return null;
+    }
+  }
+}
