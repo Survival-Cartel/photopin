cf100184425664ab35868e61777656b6b2587256
Junsung Choi <choiexe1@gmail.com>
2025-05-12 21:00:18 +0900

    feat: content type을 인자로 받도록 변경

--- lib/storage/data/data_source/storage_data_source.dart
+++ lib/storage/data/data_source/storage_data_source.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/services.dart';
 
 abstract interface class StorageDataSource {
-  Future<void> uploadFile(String path, Uint8List bytes);
+  Future<void> uploadFile(String path, Uint8List bytes, String? contentType);
   Future<String> getDownloadUrl(String path);
 }
