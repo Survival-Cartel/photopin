e631ed237f27ebbbe905805e3e170f6604ff3ee5
roychoi1012 <roychoi1012@naver.com>
2025-05-12 15:49:24 +0900

    feat: main view model create

--- lib/presentation/screen/main/main_view_model.dart
+++ lib/presentation/screen/main/main_view_model.dart
@@ -1,55 +1,48 @@
-// lib/presentation/screen/main/main_screen_view_model.dart
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 import 'package:photopin/core/usecase/get_current_user_use_case.dart';
 import 'package:photopin/presentation/screen/main/main_state.dart';
 import 'package:photopin/user/domain/model/user_model.dart';
 
-class MainScreenViewModel {
-  final GetCurrentUserUseCase _getCurrentUserUseCase;
-  final StatefulNavigationShell navigationShell;
+class MainScreenViewModel with ChangeNotifier {
+  final GetCurrentUserUseCase getCurrentUserUseCase;
+  MainState _state = const MainState();
 
-  ValueNotifier<MainScreenState> stateNotifier = ValueNotifier<MainScreenState>(
-    const MainScreenState(),
-  );
+  MainScreenViewModel({required this.getCurrentUserUseCase});
 
-  MainScreenViewModel({
-    required this.navigationShell,
-    required GetCurrentUserUseCase getCurrentUserUseCase,
-  }) : _getCurrentUserUseCase = getCurrentUserUseCase {
-    _loadProfileImage();
-  }
+  MainState get state => _state;
 
-  MainScreenState get state => stateNotifier.value;
+  Future<void> loadProfile() async {
+    _state = state.copyWith(isLoading: true);
+    notifyListeners();
 
-  Future<void> _loadProfileImage() async {
     try {
-      final UserModel user = await _getCurrentUserUseCase.execute();
-
-      final profileImg = user.profileImg;
-      if (profileImg.isNotEmpty) {
-        stateNotifier.value = stateNotifier.value.copyWith(
-          profileImageUrl: profileImg,
-        );
-      }
+      final UserModel user = await getCurrentUserUseCase.execute();
+      _state = state.copyWith(isLoading: false, userModel: user);
+      notifyListeners();
     } catch (e) {
-      debugPrint('프로필 이미지 로드 중 오류 발생: $e');
+      // 에러 처리
+      _state = state.copyWith(isLoading: false);
+      notifyListeners();
+      // 추가적인 에러 처리 로직이 필요할 수 있음
     }
   }
 
-  void changeTab(int index) {
-    navigationShell.goBranch(index);
-    stateNotifier.value = stateNotifier.value.copyWith(selectedIndex: index);
-  }
-
-  String? get profileImgToDisplay {
-    final profileImageUrl = state.profileImageUrl;
-    return (profileImageUrl == null || profileImageUrl.isEmpty)
-        ? null
-        : profileImageUrl;
-  }
-
-  void dispose() {
-    stateNotifier.dispose();
+  Future<void> init() async {
+    _state = state.copyWith(isLoading: true);
+    notifyListeners();
+    try {
+      final UserModel user = await getCurrentUserUseCase.execute();
+      _state = state.copyWith(
+        userModel: user,
+        selectedIndex: 0,
+        isLoading: false,
+      );
+      notifyListeners();
+    } catch (e) {
+      // 에러 처리
+      _state = state.copyWith(isLoading: false);
+      notifyListeners();
+      // 추가적인 에러 처리 로직이 필요할 수 있음
+    }
   }
 }
