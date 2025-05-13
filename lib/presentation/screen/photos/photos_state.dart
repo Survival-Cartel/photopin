313de8004930bc11725c9f88af79134f6964e4b7
roychoi1012 <roychoi1012@naver.com>
2025-05-13 17:25:41 +0900

    refactor: photos state modify

--- lib/presentation/screen/photos/photos_state.dart
+++ lib/presentation/screen/photos/photos_state.dart
@@ -1,16 +1,27 @@
 import 'package:photopin/journal/domain/model/journal_model.dart';
+import 'package:photopin/photo/domain/model/photo_model.dart';
 
 class PhotosState {
   final bool isLoading;
   final List<JournalModel> journals;
+  final List<PhotoModel> photos;
 
-  PhotosState({this.isLoading = false, List<JournalModel> journals = const []})
-    : journals = List.unmodifiable(journals);
+  PhotosState({
+    this.isLoading = false,
+    List<JournalModel> journals = const [],
+    List<PhotoModel> photos = const [],
+  }) : journals = List.unmodifiable(journals),
+       photos = List.unmodifiable(photos);
 
-  PhotosState copyWith({bool? isLoading, List<JournalModel>? journals}) {
+  PhotosState copyWith({
+    bool? isLoading,
+    List<JournalModel>? journals,
+    List<PhotoModel>? photos,
+  }) {
     return PhotosState(
       isLoading: isLoading ?? this.isLoading,
       journals: journals != null ? List.unmodifiable(journals) : this.journals,
+      photos: photos != null ? List.unmodifiable(photos) : this.photos,
     );
   }
 }
