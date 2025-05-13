43f81d14d95f4d93be67c64e1bbc892accf27910
roychoi1012 <roychoi1012@naver.com>
2025-05-13 17:26:04 +0900

    refactor: photos screen root modify

--- lib/presentation/screen/photos/photos_screen_root.dart
+++ lib/presentation/screen/photos/photos_screen_root.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:photopin/presentation/screen/photos/photos_action.dart';
+import 'package:photopin/presentation/screen/photos/photos_screen.dart';
 import 'package:photopin/presentation/screen/photos/photos_view_model.dart';
 
 class PhotosScreenRoot extends StatefulWidget {
@@ -11,13 +13,34 @@ class PhotosScreenRoot extends StatefulWidget {
 
 class _PhotosScreenRootState extends State<PhotosScreenRoot> {
   @override
+  void initState() {
+    super.initState();
+    widget.viewModel.init();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return ListenableBuilder(
       listenable: widget.viewModel,
       builder: (context, child) {
         return PhotosScreen(
-          
-        )
+          state: widget.viewModel.state,
+          onAction: (action) {
+            switch (action) {
+              case PhotoFilterClick():
+                // TODO: Handle this case.
+                throw UnimplementedError();
+              case PhotoCardClick():
+                // TODO: Handle this case.
+                throw UnimplementedError();
+              case PhotoApplyClick():
+                widget.viewModel.onAction(action);
+              case PhotoShareClick():
+                // TODO: Handle this case.
+                throw UnimplementedError();
+            }
+          },
+        );
       },
     );
   }
