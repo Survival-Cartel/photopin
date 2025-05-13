51833549f13ddcc70c078e91ee0625372453e801
roychoi1012 <roychoi1012@naver.com>
2025-05-13 17:27:30 +0900

    modify: map filter modify

--- lib/presentation/component/map_filter.dart
+++ lib/presentation/component/map_filter.dart
@@ -38,74 +38,83 @@ class _MapFilterState extends State<MapFilter> {
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
-      height: 24,
+      height: 24, // 세로 사이즈 고정 유지
       decoration: BoxDecoration(
         color: Colors.transparent,
         borderRadius: BorderRadius.circular(30),
       ),
       child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly, // 버튼들을 균등하게 분배
         children: List.generate(widget.icons.length, (index) {
           final bool isLastItem = index == widget.icons.length - 1;
 
-          return Expanded(
-            child: Padding(
-              padding: EdgeInsets.only(right: isLastItem ? 0 : 8),
-              child: GestureDetector(
-                onTap: () {
-                  setState(() {
-                    selectedIndex = index;
-                  });
-                  widget.onSelected(index);
-                },
-                child: Container(
-                  height: double.infinity,
-                  decoration: BoxDecoration(
-                    color:
-                        selectedIndex == index
-                            ? AppColors.primary100
-                            : AppColors.gray4,
-                    borderRadius: BorderRadius.circular(26),
-                    boxShadow:
-                        selectedIndex == index
-                            ? const [
-                              BoxShadow(
-                                color: Colors.black12,
-                                spreadRadius: 1,
-                                blurRadius: 2,
-                                offset: Offset(0, 1.5),
+          return Padding(
+            // Expanded 대신 Padding 사용
+            padding: EdgeInsets.only(right: isLastItem ? 0 : 8),
+            child: GestureDetector(
+              onTap: () {
+                setState(() {
+                  selectedIndex = index;
+                });
+                widget.onSelected(index);
+              },
+              child: Container(
+                // 높이는 부모 컨테이너에 맞추기
+                height: double.infinity,
+                decoration: BoxDecoration(
+                  color:
+                      selectedIndex == index
+                          ? AppColors.primary100
+                          : AppColors.gray4,
+                  borderRadius: BorderRadius.circular(26),
+                  boxShadow:
+                      selectedIndex == index
+                          ? const [
+                            BoxShadow(
+                              color: Colors.black12,
+                              spreadRadius: 1,
+                              blurRadius: 2,
+                              offset: Offset(0, 1.5),
+                            ),
+                          ]
+                          : null,
+                ),
+                // IntrinsicWidth를 사용하여 내용물 크기에 맞게 크기 조정
+                child: IntrinsicWidth(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      // 가로 패딩 추가
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 11),
+                        child: Row(
+                          mainAxisSize: MainAxisSize.min, // 내용물에 맞게 크기 조정
+                          children: [
+                            IconTheme(
+                              data: IconThemeData(
+                                color:
+                                    selectedIndex == index
+                                        ? Colors.white
+                                        : AppColors.gray1,
+                                size: 16,
                               ),
-                            ]
-                            : null,
-                  ),
-                  child: Align(
-                    alignment: Alignment.center,
-                    child: Row(
-                      mainAxisSize: MainAxisSize.min,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        IconTheme(
-                          data: IconThemeData(
-                            color:
-                                selectedIndex == index
-                                    ? Colors.white
-                                    : AppColors.gray1,
-                            size: 16,
-                          ),
-                          child: widget.icons[index],
+                              child: widget.icons[index],
+                            ),
+                            const SizedBox(width: 4),
+                            Text(
+                              widget.labels[index],
+                              style: TextStyle(
+                                fontSize: 12,
+                                color:
+                                    selectedIndex == index
+                                        ? Colors.white
+                                        : AppColors.gray1,
+                              ),
+                            ),
+                          ],
                         ),
-                        const SizedBox(width: 4),
-                        Text(
-                          widget.labels[index],
-                          style: TextStyle(
-                            fontSize: 12,
-                            color:
-                                selectedIndex == index
-                                    ? Colors.white
-                                    : AppColors.gray1,
-                          ),
-                        ),
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
                 ),
               ),
