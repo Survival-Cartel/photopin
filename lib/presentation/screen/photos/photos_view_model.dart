9e7945c59aae701d45917a77c1cb8f3e1904bb3d
roychoi1012 <roychoi1012@naver.com>
2025-05-13 17:25:27 +0900

    refactor: photos view model modify

--- lib/presentation/screen/photos/photos_view_model.dart
+++ lib/presentation/screen/photos/photos_view_model.dart
@@ -1,15 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:photopin/core/domain/journal_photo_collection.dart';
 import 'package:photopin/core/usecase/get_journal_list_use_case.dart';
+import 'package:photopin/core/usecase/get_photo_list_use_case.dart';
+import 'package:photopin/photo/data/repository/photo_repository.dart';
+import 'package:photopin/photo/domain/model/photo_model.dart';
 import 'package:photopin/presentation/screen/photos/photos_action.dart';
 import 'package:photopin/presentation/screen/photos/photos_state.dart';
 
 class PhotosViewModel with ChangeNotifier {
-  final GetJournalListUseCase getJournalListUseCase;
+  final GetPhotoListUseCase _getPhotoListUseCase;
+  final GetJournalListUseCase _getJournalListUseCase;
+  final PhotoRepository _photoRepository;
   PhotosState _state = PhotosState();
 
-  PhotosViewModel({required this.getJournalListUseCase});
+  PhotosViewModel({
+    required GetPhotoListUseCase getPhotoListUseCase,
+    required GetJournalListUseCase getJournalListUseCase,
+    required PhotoRepository photoRepository,
+  }) : _getPhotoListUseCase = getPhotoListUseCase,
+       _getJournalListUseCase = getJournalListUseCase,
+       _photoRepository = photoRepository;
 
   PhotosState get state => _state;
+
+  Future<void> _photoApplyClick(PhotoModel photoModel) async {
+    await _photoRepository.updatePhoto(photoModel);
+  }
+
+  Future<void> init() async {
+    _state = _state.copyWith(isLoading: true);
+    notifyListeners();
+
+    List<PhotoModel> photos = await _getPhotoListUseCase.execute();
+    final JournalPhotoCollection collection =
+        await _getJournalListUseCase.execute();
+
+    _state = _state.copyWith(
+      photos: photos,
+      journals: collection.journals,
+      isLoading: false,
+    );
+    notifyListeners();
+  }
 
   Future<void> onAction(PhotosAction action) async {
     switch (action) {
@@ -20,8 +52,7 @@ class PhotosViewModel with ChangeNotifier {
         // TODO: Handle this case.
         throw UnimplementedError();
       case PhotoApplyClick():
-        // TODO: Handle this case.
-        throw UnimplementedError();
+        _photoApplyClick(action.photoModel);
       case PhotoShareClick():
         // TODO: Handle this case.
         throw UnimplementedError();
