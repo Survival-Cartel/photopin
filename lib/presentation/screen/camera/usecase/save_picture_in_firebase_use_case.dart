1426285ecb67526bc67387244ece466b477681aa
Junsung Choi <choiexe1@gmail.com>
2025-05-14 15:06:35 +0900

    feat: 파이어베이스(firestore, firebase storage)에 저장하는 유즈케이스

---
+++ lib/presentation/screen/camera/usecase/save_picture_in_firebase_use_case.dart
@@ -0,0 +1,54 @@
+import 'package:photopin/core/domain/binary_data.dart';
+import 'package:photopin/core/domain/location.dart';
+import 'package:photopin/core/enums/image_mime.dart';
+import 'package:photopin/core/usecase/get_current_location_use_case.dart';
+import 'package:photopin/core/usecase/get_place_name_use_case.dart';
+import 'package:photopin/core/usecase/save_photo_use_case.dart';
+import 'package:photopin/core/usecase/upload_file_use_case.dart';
+import 'package:photopin/photo/data/dto/photo_dto.dart';
+import 'package:uuid/v4.dart';
+
+class SavePictureInFirebaseUseCase {
+  final GetCurrentLocationUseCase _getCurrentLocationUseCase;
+  final UploadFileUseCase _uploadFileUseCase;
+  final GetPlaceNameUseCase _getPlaceNameUseCase;
+  final SavePhotoUseCase _savePhotoUseCase;
+
+  final UuidV4 uuid = const UuidV4();
+
+  const SavePictureInFirebaseUseCase({
+    required getCurrentLocationUseCase,
+    required uploadFileUseCase,
+    required getPlaceNameUseCase,
+    required savePhotoUseCase,
+  }) : _getCurrentLocationUseCase = getCurrentLocationUseCase,
+       _uploadFileUseCase = uploadFileUseCase,
+       _getPlaceNameUseCase = getPlaceNameUseCase,
+       _savePhotoUseCase = savePhotoUseCase;
+
+  Future<void> execute(BinaryData binaryData, ImageMime imageMime) async {
+    final Location? location = await _getCurrentLocationUseCase.execute();
+
+    if (location != null) {
+      final String downloadUrl = await _uploadFileUseCase.execute(
+        uuid.generate(),
+        binaryData.bytes,
+        imageMime,
+      );
+
+      final String placeName = await _getPlaceNameUseCase.execute(
+        location: location,
+      );
+
+      PhotoDto dto = PhotoDto(
+        name: placeName,
+        imageUrl: downloadUrl,
+        latitude: location.latitude,
+        longitude: location.longitude,
+        dateTimeMilli: DateTime.now().millisecondsSinceEpoch,
+      );
+
+      _savePhotoUseCase.execute(dto);
+    }
+  }
+}
