55b2622f223126dc464231c9edf426be2bfb9072
roychoi1012 <roychoi1012@naver.com>
2025-05-14 11:34:49 +0900

    feat: map filter 아이콘 제거 수정

--- lib/presentation/component/map_filter.dart
+++ lib/presentation/component/map_filter.dart
@@ -1,74 +1,65 @@
 import 'package:flutter/material.dart';
 import 'package:photopin/core/styles/app_color.dart';
 
-/// [labels]와 [icons] 리스트의 각 요소는 1:1로 매칭되어야 하며, 그렇지 않을 경우 ArgumentError가 발생합니다.
-/// 즉, [labels]의 길이와 [icons]의 길이가 같아야 합니다.
 class MapFilter extends StatefulWidget {
-  final List<Icon> icons;
   final List<String> labels;
-  final int initialIndex;
+  final int selectedIndex;
   final Function(int) onSelected;
 
-  MapFilter({
+  const MapFilter({
     super.key,
-    required this.icons,
     required this.labels,
-    required this.initialIndex,
+    required this.selectedIndex,
     required this.onSelected,
-  }) {
-    if (labels.length != icons.length) {
-      throw ArgumentError('labels와 icons의 길이가 같아야 합니다.');
-    }
-  }
+  });
 
   @override
   State<MapFilter> createState() => _MapFilterState();
 }
 
 class _MapFilterState extends State<MapFilter> {
-  late int selectedIndex;
+  late int _currentIndex;
 
   @override
   void initState() {
     super.initState();
-    selectedIndex = widget.initialIndex;
+    _currentIndex = widget.selectedIndex;
+  }
+
+  @override
+  void didUpdateWidget(MapFilter oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    // 부모 위젯에서 selectedIndex가 변경되면 내부 상태도 업데이트
+    if (oldWidget.selectedIndex != widget.selectedIndex) {
+      _currentIndex = widget.selectedIndex;
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      height: 24, // 세로 사이즈 고정 유지
-      decoration: BoxDecoration(
-        color: Colors.transparent,
-        borderRadius: BorderRadius.circular(30),
-      ),
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
       child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly, // 버튼들을 균등하게 분배
-        children: List.generate(widget.icons.length, (index) {
-          final bool isLastItem = index == widget.icons.length - 1;
-
+        children: List.generate(widget.labels.length, (index) {
           return Padding(
-            // Expanded 대신 Padding 사용
-            padding: EdgeInsets.only(right: isLastItem ? 0 : 8),
+            padding: const EdgeInsets.only(right: 8),
             child: GestureDetector(
               onTap: () {
                 setState(() {
-                  selectedIndex = index;
+                  _currentIndex = index;
                 });
                 widget.onSelected(index);
               },
               child: Container(
-                // 높이는 부모 컨테이너에 맞추기
-                height: double.infinity,
+                height: 24, // 높이는 고정
                 decoration: BoxDecoration(
                   color:
-                      selectedIndex == index
+                      _currentIndex == index
                           ? AppColors.primary100
                           : AppColors.gray4,
                   borderRadius: BorderRadius.circular(26),
                   boxShadow:
-                      selectedIndex == index
+                      _currentIndex == index
                           ? const [
                             BoxShadow(
                               color: Colors.black12,
@@ -79,42 +70,19 @@ class _MapFilterState extends State<MapFilter> {
                           ]
                           : null,
                 ),
-                // IntrinsicWidth를 사용하여 내용물 크기에 맞게 크기 조정
-                child: IntrinsicWidth(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      // 가로 패딩 추가
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 11),
-                        child: Row(
-                          mainAxisSize: MainAxisSize.min, // 내용물에 맞게 크기 조정
-                          children: [
-                            IconTheme(
-                              data: IconThemeData(
-                                color:
-                                    selectedIndex == index
-                                        ? Colors.white
-                                        : AppColors.gray1,
-                                size: 16,
-                              ),
-                              child: widget.icons[index],
-                            ),
-                            const SizedBox(width: 4),
-                            Text(
-                              widget.labels[index],
-                              style: TextStyle(
-                                fontSize: 12,
-                                color:
-                                    selectedIndex == index
-                                        ? Colors.white
-                                        : AppColors.gray1,
-                              ),
-                            ),
-                          ],
-                        ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 12),
+                  child: Center(
+                    child: Text(
+                      widget.labels[index],
+                      style: TextStyle(
+                        fontSize: 12,
+                        color:
+                            _currentIndex == index
+                                ? Colors.white
+                                : AppColors.gray1,
                       ),
-                    ],
+                    ),
                   ),
                 ),
               ),
