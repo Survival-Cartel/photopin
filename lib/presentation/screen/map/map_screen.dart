894c143976d2759070079a1cb3a501ceabb2c0c5
JaeHwan <wpghks56@naver.com>
2025-05-14 16:56:33 +0900

    refactor: bottom sheet 최소로 줄이는 크기 늘리기

--- lib/presentation/screen/map/map_screen.dart
+++ lib/presentation/screen/map/map_screen.dart
@@ -4,18 +4,16 @@ import 'package:flutter/material.dart';
 import 'package:google_maps_cluster_manager_2/google_maps_cluster_manager_2.dart'
     as package;
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:grouped_list/grouped_list.dart';
 import 'package:photopin/core/domain/photo_cluster_item.dart';
-import 'package:photopin/core/extensions/datetime_extension.dart';
 import 'package:photopin/core/styles/app_color.dart';
 import 'package:photopin/core/styles/app_font.dart';
 import 'package:photopin/journal/domain/model/journal_model.dart';
 import 'package:photopin/photo/domain/model/photo_model.dart';
 import 'package:photopin/presentation/component/custom_map_marker.dart';
 import 'package:photopin/presentation/component/date_range_slider.dart';
+import 'package:photopin/presentation/component/grouplist_photos_timeline_tile.dart';
 import 'package:photopin/presentation/component/photopin_map.dart';
 import 'package:photopin/presentation/component/text_chip.dart';
-import 'package:photopin/presentation/component/timeline_tile.dart';
 import 'package:photopin/presentation/component/trip_with_chips.dart';
 import 'package:photopin/presentation/screen/map/map_action.dart';
 import 'package:photopin/presentation/screen/map/map_state.dart';
@@ -32,7 +30,7 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
-  static const double _minHeight = 104;
+  static const double _minHeight = 160;
   late package.ClusterManager<PhotoClusterItem> _clusterManager;
   final Completer<GoogleMapController> _controller =
       Completer<GoogleMapController>();
@@ -331,44 +329,9 @@ class MapBottomDragWidget extends StatelessWidget {
             ],
           ),
           Expanded(
-            child: GroupedListView<PhotoModel, DateTime>(
-              elements: photos,
-              groupBy:
-                  (photo) => DateTime(
-                    photo.dateTime.year,
-                    photo.dateTime.month,
-                    photo.dateTime.day,
-                  ),
-              groupSeparatorBuilder:
-                  (date) => Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Row(
-                      spacing: 8,
-                      children: [
-                        Expanded(
-                          child: Container(height: 1, color: AppColors.gray4),
-                        ),
-                        Text(
-                          date.formDateString(),
-                          style: AppFonts.smallTextRegular,
-                        ),
-                        Expanded(
-                          child: Container(height: 1, color: AppColors.gray4),
-                        ),
-                      ],
-                    ),
-                  ),
-              itemBuilder: (context, photo) {
-                return TimeLineTile(
-                  photoId: photo.id,
-                  dateTime: photo.dateTime,
-                  title: photo.name,
-                  imageUrl: photo.imageUrl,
-                  onTap: (id) => onAction(MapAction.onPhotoClick(id)),
-                );
-              },
-              separator: const SizedBox(height: 4),
-              order: GroupedListOrder.ASC,
+            child: GrouplistPhotosTimelineTile(
+              photos: photos,
+              onTap: (id) => onAction(MapAction.onPhotoClick(id)),
             ),
           ),
         ],
