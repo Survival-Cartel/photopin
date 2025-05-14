f792d86e417dd52b15e80ef6307c4ea86bf97f3d
JaeHwan <wpghks56@naver.com>
2025-05-14 16:52:49 +0900

    refactor: compare model name change integration model

--- lib/core/domain/integration_model.dart
+++ lib/core/domain/integration_model.dart
@@ -2,12 +2,12 @@ import 'package:photopin/journal/domain/model/journal_model.dart';
 import 'package:photopin/photo/domain/model/photo_model.dart';
 import 'package:photopin/user/domain/model/user_model.dart';
 
-class CompareModel {
+class IntegrationModel {
   final UserModel user;
   final JournalModel journal;
   final List<PhotoModel> photos;
 
-  const CompareModel({
+  const IntegrationModel({
     required this.user,
     required this.journal,
     required this.photos,
