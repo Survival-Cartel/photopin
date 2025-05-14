c4d6a6a71a411da8d86c9278ba037019c0a3ab48
Junsung Choi <choiexe1@gmail.com>
2025-05-14 15:05:25 +0900

    feat: permission check usecase가 permission allow type을 반환하도록 변경

--- lib/core/usecase/permission_check_use_case.dart
+++ lib/core/usecase/permission_check_use_case.dart
@@ -1,23 +1,50 @@
 import 'package:permission_handler/permission_handler.dart';
+import 'package:photopin/core/enums/permission_allow_status.dart';
 import 'package:photopin/core/enums/permission_type.dart';
 
 class PermissionCheckUseCase {
   PermissionCheckUseCase();
 
-  Future<void> execute(PermissionType permission) async {
+  Future<PermissionAllowStatus> execute(PermissionType permission) async {
     switch (permission) {
       case PermissionType.camera:
-        await Permission.camera.onPermanentlyDeniedCallback(() {
-          openAppSettings();
-        }).request();
+        final PermissionStatus status =
+            await Permission.camera.onPermanentlyDeniedCallback(() {
+              openAppSettings();
+            }).request();
+
+        return _handleStatus(status);
       case PermissionType.photos:
-        await Permission.photos.onPermanentlyDeniedCallback(() {
-          openAppSettings();
-        }).request();
+        final PermissionStatus status =
+            await Permission.photos.onPermanentlyDeniedCallback(() {
+              openAppSettings();
+            }).request();
+
+        return _handleStatus(status);
       case PermissionType.location:
-        await Permission.location.onPermanentlyDeniedCallback(() {
-          openAppSettings();
-        }).request();
+        final PermissionStatus status =
+            await Permission.location.onPermanentlyDeniedCallback(() {
+              openAppSettings();
+            }).request();
+
+        return _handleStatus(status);
+    }
+  }
+
+  PermissionAllowStatus _handleStatus(PermissionStatus status) {
+    switch (status) {
+      case PermissionStatus.denied:
+        return PermissionAllowStatus.denied;
+      case PermissionStatus.granted:
+        return PermissionAllowStatus.allow;
+      case PermissionStatus.restricted:
+        return PermissionAllowStatus.restricted;
+      case PermissionStatus.limited:
+        return PermissionAllowStatus.limited;
+      case PermissionStatus.permanentlyDenied:
+        return PermissionAllowStatus.denied;
+      case PermissionStatus.provisional:
+        return PermissionAllowStatus.denied;
     }
   }
 }
