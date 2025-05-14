35f52fcbc6e4cfdd927eb2f7c65afe01eb3e590a
roychoi1012 <roychoi1012@naver.com>
2025-05-14 11:32:54 +0900

    feat: photos state modify

--- lib/presentation/screen/photos/photos_state.dart
+++ lib/presentation/screen/photos/photos_state.dart
@@ -3,11 +3,13 @@ import 'package:photopin/photo/domain/model/photo_model.dart';
 
 class PhotosState {
   final bool isLoading;
+  final int selectedFilterIndex;
   final List<JournalModel> journals;
   final List<PhotoModel> photos;
 
   PhotosState({
     this.isLoading = false,
+    this.selectedFilterIndex = 0,
     List<JournalModel> journals = const [],
     List<PhotoModel> photos = const [],
   }) : journals = List.unmodifiable(journals),
@@ -15,11 +17,13 @@ class PhotosState {
 
   PhotosState copyWith({
     bool? isLoading,
+    int? selectedFilterIndex,
     List<JournalModel>? journals,
     List<PhotoModel>? photos,
   }) {
     return PhotosState(
       isLoading: isLoading ?? this.isLoading,
+      selectedFilterIndex: selectedFilterIndex ?? this.selectedFilterIndex,
       journals: journals != null ? List.unmodifiable(journals) : this.journals,
       photos: photos != null ? List.unmodifiable(photos) : this.photos,
     );
