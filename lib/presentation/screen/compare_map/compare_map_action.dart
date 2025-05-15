9f3eee9473b62e1d0a3f13ce884f72f06a8f1b25
JaeHwan <wpghks56@naver.com>
2025-05-15 10:56:43 +0900

    add: marker 및 타일 클릭시 이벤트 추가

--- lib/presentation/screen/compare_map/compare_map_action.dart
+++ lib/presentation/screen/compare_map/compare_map_action.dart
@@ -1,1 +1,16 @@
-sealed class CompareMapAction {}
+import 'package:flutter/foundation.dart';
+
+sealed class CompareMapAction {
+  const factory CompareMapAction.onPhotoClick({
+    required String photoId,
+    required bool isCompare,
+  }) = OnPhotoClick;
+}
+
+@immutable
+class OnPhotoClick implements CompareMapAction {
+  final String photoId;
+  final bool isCompare;
+
+  const OnPhotoClick({required this.photoId, required this.isCompare});
+}
