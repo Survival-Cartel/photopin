1cfd73562c86ce89eb1b044dea7faddd50439f90
Junsung Choi <choiexe1@gmail.com>
2025-05-15 14:52:20 +0900

    feat: showEditButton 및 수정 버튼 추가

--- lib/presentation/component/journal_card.dart
+++ lib/presentation/component/journal_card.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:photopin/core/extensions/datetime_extension.dart';
 import 'package:photopin/core/styles/app_color.dart';
 import 'package:photopin/core/styles/app_font.dart';
 import 'package:photopin/journal/domain/model/journal_model.dart';
 import 'package:photopin/presentation/component/journal_card_image.dart';
 
+// [showEditButton]과 [onTapEdit]은 함께 되어야 합니다.
 class JournalCard extends StatelessWidget {
   final JournalModel journal;
   final Function(String journalId) onTap;
+  final Function(String journalId)? onTapEdit;
   final String? imageUrl;
   final int photoCount;
+  final bool showEditButton;
 
   const JournalCard({
     super.key,
@@ -17,27 +21,56 @@ class JournalCard extends StatelessWidget {
     required this.journal,
     required this.imageUrl,
     required this.photoCount,
+    this.showEditButton = false,
+    this.onTapEdit = null,
   });
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () => onTap(journal.id),
-      behavior: HitTestBehavior.opaque,
-      child: Column(
-        children: [
-          SizedBox(
-            height: 144,
-            child: JournalCardImage(
-              imageUrl:
-                  imageUrl ??
-                  'https://web.cau.ac.kr/_images/_board/skin/album/1//no_image.gif',
-              journeyTitle: journal.name,
-              description: journal.comment,
-              bottomRadius: false,
-            ),
+    return Column(
+      children: [
+        SizedBox(
+          height: 144,
+          child: Stack(
+            children: [
+              Positioned(
+                child: GestureDetector(
+                  onTap: () => onTap(journal.id),
+                  behavior: HitTestBehavior.opaque,
+                  child: JournalCardImage(
+                    imageUrl:
+                        imageUrl ??
+                        'https://web.cau.ac.kr/_images/_board/skin/album/1//no_image.gif',
+                    journeyTitle: journal.name,
+                    description: journal.comment,
+                    bottomRadius: false,
+                  ),
+                ),
+              ),
+              Positioned(
+                top: 15,
+                right: 15,
+                child: GestureDetector(
+                  onTap: () => onTapEdit?.call(journal.id),
+                  behavior: HitTestBehavior.opaque,
+                  child: CircleAvatar(
+                    maxRadius: 18,
+                    backgroundColor: AppColors.black.withValues(alpha: 0.4),
+                    child: Icon(
+                      Icons.edit,
+                      color: AppColors.primary60,
+                      size: 20,
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
-          Container(
+        ),
+        GestureDetector(
+          onTap: () => onTap(journal.id),
+          behavior: HitTestBehavior.opaque,
+          child: Container(
             height: 40,
             padding: const EdgeInsets.symmetric(horizontal: 12),
             decoration: const BoxDecoration(
@@ -62,8 +95,8 @@ class JournalCard extends StatelessWidget {
               ],
             ),
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
