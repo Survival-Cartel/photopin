10734391c1898c500512e4b81874bf54067a8232
JaeHwan <wpghks56@naver.com>
2025-05-15 19:06:05 +0900

    refactor: 불필요한 액션 삭제

--- lib/presentation/screen/map/map_action.dart
+++ lib/presentation/screen/map/map_action.dart
@@ -7,9 +7,6 @@ sealed class MapAction {
     required DateTime endDate,
   }) = OnDateRangeClick;
   const factory MapAction.onPhotoClick(String photoId) = OnPhotoClick;
-  const factory MapAction.onCancelClick() = OnCancelClick;
-  const factory MapAction.onApplyFilterClick() = OnApplyFilterClick;
-  const factory MapAction.onEditClick() = OnEditClick;
   const factory MapAction.onShareClick() = OnShareClick;
 }
 
@@ -31,21 +28,6 @@ class OnPhotoClick implements MapAction {
   final String photoId;
 
   const OnPhotoClick(this.photoId);
-}
-
-@immutable
-class OnCancelClick implements MapAction {
-  const OnCancelClick();
-}
-
-@immutable
-class OnApplyFilterClick implements MapAction {
-  const OnApplyFilterClick();
-}
-
-@immutable
-class OnEditClick implements MapAction {
-  const OnEditClick();
 }
 
 @immutable
