1913e0b973d43507bda2452336443a01e89460f6
Junsung Choi <choiexe1@gmail.com>
2025-05-16 23:12:24 +0900

    feat: 날짜 범위로 저널 검색

--- lib/journal/data/data_source/journal_data_source_impl.dart
+++ lib/journal/data/data_source/journal_data_source_impl.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
 import 'package:photopin/core/errors/firestore_error.dart';
 import 'package:photopin/journal/data/data_source/journal_data_source.dart';
 import 'package:photopin/journal/data/dto/journal_dto.dart';
@@ -87,5 +88,26 @@ class JournalDataSourceImpl implements JournalDataSource {
   @override
   Future<void> update(JournalDto journal) async {
     await _journalStore.doc(journal.id).set(journal, SetOptions(merge: true));
+  }
+
+  @override
+  Future<List<JournalDto>> findJournalsByDateTimeRange(
+    DateTimeRange range,
+  ) async {
+    try {
+      final snapshot = await _journalStore
+          .where('startDate', isGreaterThanOrEqualTo: range.start)
+          .where('endDate', isLessThanOrEqualTo: range.end)
+          .get()
+          .timeout(
+            const Duration(seconds: 8),
+            onTimeout: () => throw FirestoreError.timeOutError,
+          );
+
+      return snapshot.docs.map((doc) => doc.data()).toList();
+    } catch (e) {
+      // 필요 시 다른 에러 핸들링 가능
+      rethrow;
+    }
   }
 }
