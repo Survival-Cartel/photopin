2d24b6dc9e28cbd4513fd2691912ac54df8e1713
Junsung Choi <choiexe1@gmail.com>
2025-05-16 22:56:58 +0900

    feat: date time range 상태 추가

--- lib/presentation/screen/journal/journal_state.dart
+++ lib/presentation/screen/journal/journal_state.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:photopin/core/enums/search_filter_option.dart';
 import 'package:photopin/journal/domain/model/journal_model.dart';
 import 'package:photopin/photo/domain/model/photo_model.dart';
 
@@ -5,21 +7,32 @@ class JournalState {
   final bool isLoading;
   final List<JournalModel> journals;
   final Map<String, List<PhotoModel>> photoMap;
+  final DateTimeRange searchDateTimeRange;
+  final SearchFilterOption searchFilterOption;
 
   JournalState({
     this.isLoading = false,
     List<JournalModel> journals = const [],
+    DateTimeRange? searchDateTimeRange,
+    this.searchFilterOption = SearchFilterOption.title,
     Map<String, List<PhotoModel>> photoMap = const {},
   }) : journals = List.unmodifiable(journals),
-       photoMap = Map.unmodifiable(photoMap);
+       photoMap = Map.unmodifiable(photoMap),
+       searchDateTimeRange =
+           searchDateTimeRange ??
+           DateTimeRange(start: DateTime.now(), end: DateTime.now());
 
   JournalState copyWith({
     bool? isLoading,
     List<JournalModel>? journals,
     Map<String, List<PhotoModel>>? photoMap,
+    SearchFilterOption? searchFilterOption,
+    DateTimeRange? searchDateTimeRange,
   }) {
     return JournalState(
       isLoading: isLoading ?? this.isLoading,
+      searchFilterOption: searchFilterOption ?? this.searchFilterOption,
+      searchDateTimeRange: searchDateTimeRange ?? this.searchDateTimeRange,
       journals: journals != null ? List.unmodifiable(journals) : this.journals,
       photoMap: photoMap != null ? Map.unmodifiable(photoMap) : this.photoMap,
     );
