a391311792b1ae22084e3f433900348b3aa6229d
roychoi1012 <roychoi1012@naver.com>
2025-05-16 16:24:41 +0900

    feat: photos view model modify

--- lib/presentation/screen/photos/photos_view_model.dart
+++ lib/presentation/screen/photos/photos_view_model.dart
@@ -1,8 +1,12 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:photopin/core/domain/journal_photo_collection.dart';
 import 'package:photopin/core/usecase/get_journal_list_use_case.dart';
 import 'package:photopin/core/usecase/get_photo_list_use_case.dart';
 import 'package:photopin/core/usecase/get_photo_list_with_journal_id_use_case.dart';
+import 'package:photopin/core/usecase/watch_journals_use_case.dart';
+import 'package:photopin/core/usecase/watch_photos_use_case.dart';
 import 'package:photopin/photo/data/repository/photo_repository.dart';
 import 'package:photopin/photo/domain/model/photo_model.dart';
 import 'package:photopin/presentation/screen/photos/photos_action.dart';
@@ -13,6 +17,9 @@ class PhotosViewModel with ChangeNotifier {
   final GetJournalListUseCase _getJournalListUseCase;
   final GetPhotoListWithJournalIdUseCase _getPhotoListWithJournalIdUseCase;
   final PhotoRepository _photoRepository;
+  final WatchJournalsUseCase _watchJournalsUserCase;
+
+  StreamSubscription<JournalPhotoCollection>? _journalSubscription;
   PhotosState _state = PhotosState();
 
   PhotosViewModel({
@@ -20,12 +27,20 @@ class PhotosViewModel with ChangeNotifier {
     required GetJournalListUseCase getJournalListUseCase,
     required GetPhotoListWithJournalIdUseCase getPhotoListWithJournalIdUseCase,
     required PhotoRepository photoRepository,
+    required WatchJournalsUseCase watchJournalsUserCase,
   }) : _getPhotoListUseCase = getPhotoListUseCase,
        _getJournalListUseCase = getJournalListUseCase,
        _getPhotoListWithJournalIdUseCase = getPhotoListWithJournalIdUseCase,
+       _watchJournalsUserCase = watchJournalsUserCase,
        _photoRepository = photoRepository;
 
   PhotosState get state => _state;
+
+  @override
+  void dispose() {
+    _journalSubscription?.cancel();
+    super.dispose();
+  }
 
   Future<void> _photoApplyClick(PhotoModel photoModel) async {
     await _photoRepository.updatePhoto(photoModel);
@@ -71,16 +86,16 @@ class PhotosViewModel with ChangeNotifier {
     _state = _state.copyWith(isLoading: true);
     notifyListeners();
 
-    List<PhotoModel> photos = await _getPhotoListUseCase.execute();
-    final JournalPhotoCollection collection =
-        await _getJournalListUseCase.execute();
-
-    _state = _state.copyWith(
-      photos: photos,
-      journals: collection.journals,
-      isLoading: false,
-    );
-    notifyListeners();
+    _journalSubscription = _watchJournalsUserCase.execute().listen((
+      collection,
+    ) {
+      _state = _state.copyWith(
+        photos: collection.photoMap.values.expand((photos) => photos).toList(),
+        journals: collection.journals,
+        isLoading: false,
+      );
+      notifyListeners();
+    });
   }
 
   Future<void> onAction(PhotosAction action) async {
