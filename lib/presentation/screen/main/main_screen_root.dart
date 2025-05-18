6cde12bca18a95c56358cdbb4016a8d69f9a3574
cho1taeho <cow12dog@gmail.com>
2025-05-18 20:58:16 +0900

    feat: streamController 추가

--- lib/presentation/screen/main/main_screen_root.dart
+++ lib/presentation/screen/main/main_screen_root.dart
@@ -1,26 +1,74 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:photopin/core/stream_event/stream_event.dart';
+import 'package:photopin/core/styles/app_color.dart';
+import 'package:photopin/core/styles/app_font.dart';
 import 'package:photopin/presentation/screen/main/main_screen.dart';
 import 'package:photopin/presentation/screen/main/main_view_model.dart';
 
-class MainScreenRoot extends StatelessWidget {
+class MainScreenRoot extends StatefulWidget {
   final MainScreenViewModel viewModel;
   final StatefulNavigationShell navigationShell;
+  final StreamController<StreamEvent> streamController;
 
   const MainScreenRoot({
-    super.key,
     required this.viewModel,
     required this.navigationShell,
+    required this.streamController,
   });
+
+  @override
+  State<MainScreenRoot> createState() => _MainScreenRootState();
+}
+
+class _MainScreenRootState extends State<MainScreenRoot> {
+  StreamSubscription<StreamEvent>? _streamSubscription;
+
+  @override
+  void initState() {
+    super.initState();
+    _streamSubscription = widget.streamController.stream.listen((e) {
+      switch (e) {
+        case Success():
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text(
+                e.success.message,
+                style: AppFonts.mediumTextBold.copyWith(color: AppColors.black),
+              ),
+              backgroundColor: AppColors.secondary100,
+            ),
+          );
+        case Error():
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text(
+                e.error.message,
+                style: AppFonts.mediumTextBold.copyWith(color: AppColors.black),
+              ),
+              backgroundColor: AppColors.warningBackground,
+            ),
+          );
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _streamSubscription?.cancel();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return ListenableBuilder(
-      listenable: viewModel,
+      listenable: widget.viewModel,
       builder: (context, child) {
         return MainScreen(
-          navigationShell: navigationShell,
-          state: viewModel.state,
+          navigationShell: widget.navigationShell,
+          state: widget.viewModel.state,
         );
       },
     );
