5b00d9c54b42f4c108d6892f79f2f807cab0978b
cho1taeho <cow12dog@gmail.com>
2025-05-18 20:57:32 +0900

    feat: StreamEventSuccess enum 구현

---
+++ lib/core/enums/stream_event_success.dart
@@ -0,0 +1,13 @@
+enum StreamEventSuccess {
+  journalCreate('저널이 생성되었습니다.'),
+  journalDelete('저널이 삭제되었습니다.'),
+  journalUpdate('저널이 수정되었습니다.'),
+  photoSave('사진이 저장되었습니다.'),
+  photoDelete('사진이 삭제되었습니다.'),
+  photoUpdate('사진 정보가 변경되었습니다.'),
+  photoInJournal('사진이 저널에 추가되었습니다.');
+
+  final String message;
+
+  const StreamEventSuccess(this.message);
+}
