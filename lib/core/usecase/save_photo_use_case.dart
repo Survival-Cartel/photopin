fb014b4cb8102688b22979d930313d564240dd54
JaeHwan <wpghks56@naver.com>
2025-05-18 22:42:01 +0900

    faet: 메인 이벤트 스트림에 이벤트 추가

--- lib/core/usecase/save_photo_use_case.dart
+++ lib/core/usecase/save_photo_use_case.dart
@@ -1,13 +1,24 @@
+import 'dart:async';
+
+import 'package:photopin/core/enums/action_type.dart';
+import 'package:photopin/core/enums/error_type.dart';
+import 'package:photopin/core/stream_event/stream_event.dart';
 import 'package:photopin/photo/data/dto/photo_dto.dart';
 import 'package:photopin/photo/data/mapper/photo_mapper.dart';
 import 'package:photopin/photo/data/repository/photo_repository.dart';
 
 class SavePhotoUseCase {
   final PhotoRepository _photoRepository;
+  final StreamController<StreamEvent> streamController;
 
-  const SavePhotoUseCase(this._photoRepository);
+  const SavePhotoUseCase(this._photoRepository, this.streamController);
 
   Future<void> execute(PhotoDto dto) async {
-    await _photoRepository.savePhoto(dto.toModel());
+    try {
+      await _photoRepository.savePhoto(dto.toModel());
+      streamController.add(const StreamEvent.success(ActionType.photoSave));
+    } catch (e) {
+      streamController.add(const StreamEvent.error(ErrorType.photoSave));
+    }
   }
 }
