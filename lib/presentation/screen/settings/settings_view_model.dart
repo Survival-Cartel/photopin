388fd6894a0ca469d74cf4b417d67e83e7ed3263
roychoi1012 <roychoi1012@naver.com>
2025-05-22 15:01:36 +0900

    feat: setting view model modify

--- lib/presentation/screen/settings/settings_view_model.dart
+++ lib/presentation/screen/settings/settings_view_model.dart
@@ -1,13 +1,57 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:photopin/auth/data/repository/auth_repository.dart';
 import 'package:photopin/core/enums/permission_type.dart';
 import 'package:photopin/core/usecase/permission_check_use_case.dart';
 import 'package:photopin/presentation/screen/settings/settings_action.dart';
+import 'package:photopin/presentation/screen/settings/settings_state.dart';
+import 'package:photopin/user/domain/model/user_model.dart';
 
 class SettingsViewModel with ChangeNotifier {
   final PermissionCheckUseCase _permissionCheckUseCase;
+  final AuthRepository _authRepository;
 
-  SettingsViewModel({required PermissionCheckUseCase permissionCheckUseCase})
-    : _permissionCheckUseCase = permissionCheckUseCase;
+  SettingsState _state = const SettingsState();
+
+  SettingsState get state => _state;
+  final _eventController = StreamController<Exception?>();
+
+  Stream<Exception?> get errorStream => _eventController.stream;
+
+  @override
+  void dispose() {
+    _eventController.close();
+    super.dispose();
+  }
+
+  SettingsViewModel({
+    required PermissionCheckUseCase permissionCheckUseCase,
+    required AuthRepository authRepository,
+  }) : _permissionCheckUseCase = permissionCheckUseCase,
+       _authRepository = authRepository;
+
+  Future<void> _logout() async {
+    try {
+      _state = state.copyWith(isLoading: true);
+      notifyListeners();
+
+      await _authRepository.logout();
+      _state = state.copyWith(
+        currentUser: const UserModel(
+          displayName: '',
+          email: '',
+          id: '',
+          profileImg: '',
+        ),
+      );
+    } on Exception catch (e) {
+      _addError(e);
+    } finally {
+      _state = state.copyWith(isLoading: false);
+      notifyListeners();
+    }
+  }
 
   Future<void> onAction(SettingsAction action) async {
     switch (action) {
@@ -17,6 +61,12 @@ class SettingsViewModel with ChangeNotifier {
         await _permissionCheckUseCase.execute(PermissionType.photos);
       case LocationPermissionRequest():
         await _permissionCheckUseCase.execute(PermissionType.location);
+      case Logout():
+        await _logout();
     }
+  }
+
+  void _addError(Exception e) {
+    _eventController.add(e);
   }
 }
