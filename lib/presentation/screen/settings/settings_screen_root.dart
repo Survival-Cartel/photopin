cc7f09083746dd97e16b99820ad36c827059c6b4
cho1taeho <cow12dog@gmail.com>
2025-05-22 13:59:13 +0900

    feat: 버튼 클릭시  AlertInfo message 반환

--- lib/presentation/screen/settings/settings_screen_root.dart
+++ lib/presentation/screen/settings/settings_screen_root.dart
@@ -1,4 +1,6 @@
+import 'package:alert_info/alert_info.dart';
 import 'package:flutter/material.dart';
+import 'package:photopin/core/enums/permission_allow_status.dart';
 import 'package:photopin/presentation/screen/settings/settings_action.dart';
 import 'package:photopin/presentation/screen/settings/settings_screen.dart';
 import 'package:photopin/presentation/screen/settings/settings_view_model.dart';
@@ -19,14 +21,91 @@ class _SettingsScreenRootState extends State<SettingsScreenRoot> {
       listenable: widget.viewModel,
       builder: (BuildContext context, child) {
         return SettingsScreen(
-          onAction: (SettingsAction action) {
+          onAction: (SettingsAction action) async {
+            final result = await widget.viewModel.onAction(action);
+
             switch (action) {
               case CameraPermissionRequest():
-                widget.viewModel.onAction(action);
+                switch (result) {
+                  case PermissionAllowStatus.allow:
+                    AlertInfo.show(
+                      context: context,
+                      text: '카메라 권한이 허가되었습니다',
+                      icon: Icons.check,
+                    );
+                  case PermissionAllowStatus.denied:
+                    AlertInfo.show(
+                      context: context,
+                      text: '카메라 권한이 거부되었습니다',
+                      icon: Icons.check,
+                    );
+                  case PermissionAllowStatus.limited:
+                    AlertInfo.show(
+                      context: context,
+                      text: '카메라 권한이 제한적으로 허가되었습니다',
+                      icon: Icons.check,
+                    );
+                  case PermissionAllowStatus.restricted:
+                    AlertInfo.show(
+                      context: context,
+                      text: '카메라 권한이 제한되었습니다',
+                      icon: Icons.check,
+                    );
+                }
               case PhotoPermissionRequest():
-                widget.viewModel.onAction(action);
+                switch (result) {
+                  case PermissionAllowStatus.allow:
+                    AlertInfo.show(
+                      context: context,
+                      text: '사진 권한이 허가되었습니다',
+                      icon: Icons.check,
+                    );
+                  case PermissionAllowStatus.denied:
+                    AlertInfo.show(
+                      context: context,
+                      text: '사진 권한이 거부되었습니다',
+                      icon: Icons.check,
+                    );
+                  case PermissionAllowStatus.limited:
+                    AlertInfo.show(
+                      context: context,
+                      text: '사진 권한이 제한적으로 허가되었습니다',
+                      icon: Icons.check,
+                    );
+                  case PermissionAllowStatus.restricted:
+                    AlertInfo.show(
+                      context: context,
+                      text: '사진 권한이 제한되었습니다',
+                      icon: Icons.check,
+                    );
+                }
               case LocationPermissionRequest():
-                widget.viewModel.onAction(action);
+                switch (result) {
+                  case PermissionAllowStatus.allow:
+                    AlertInfo.show(
+                      context: context,
+                      text: '위치 권한이 허가되었습니다',
+                      icon: Icons.check,
+                    );
+                  case PermissionAllowStatus.denied:
+                    AlertInfo.show(
+                      context: context,
+                      text: '위치 권한이 거부되었습니다',
+                      icon: Icons.check,
+                    );
+                  case PermissionAllowStatus.limited:
+                    AlertInfo.show(
+                      context: context,
+                      text: '위치 권한이 제한적으로 허가되었습니다',
+                      icon: Icons.check,
+                    );
+                  case PermissionAllowStatus.restricted:
+                    AlertInfo.show(
+                      context: context,
+                      text: '위치 권한이 제한되었습니다',
+                      icon: Icons.check,
+                    );
+                }
             }
           },
         );
