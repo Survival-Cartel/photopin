ae996c1ae5c209289de1a2dde577fc429c66e2f9
cho1taeho <cow12dog@gmail.com>
2025-05-22 13:58:53 +0900

    feat: onAction PermissionAllowStatus 반환 변경

--- lib/presentation/screen/settings/settings_view_model.dart
+++ lib/presentation/screen/settings/settings_view_model.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:photopin/core/enums/permission_allow_status.dart';
 import 'package:photopin/core/enums/permission_type.dart';
 import 'package:photopin/core/usecase/permission_check_use_case.dart';
 import 'package:photopin/presentation/screen/settings/settings_action.dart';
@@ -9,14 +10,14 @@ class SettingsViewModel with ChangeNotifier {
   SettingsViewModel({required PermissionCheckUseCase permissionCheckUseCase})
     : _permissionCheckUseCase = permissionCheckUseCase;
 
-  Future<void> onAction(SettingsAction action) async {
+  Future<PermissionAllowStatus> onAction(SettingsAction action) async {
     switch (action) {
       case CameraPermissionRequest():
-        await _permissionCheckUseCase.execute(PermissionType.camera);
+        return await _permissionCheckUseCase.execute(PermissionType.camera);
       case PhotoPermissionRequest():
-        await _permissionCheckUseCase.execute(PermissionType.photos);
+        return await _permissionCheckUseCase.execute(PermissionType.photos);
       case LocationPermissionRequest():
-        await _permissionCheckUseCase.execute(PermissionType.location);
+        return await _permissionCheckUseCase.execute(PermissionType.location);
     }
   }
 }
